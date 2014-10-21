b1292c0adc8072ae8f5ac8197f615a127b8c13c9
FRC 4818 The Herd <wfrobotics@gmail.com>
2014-10-21 17:03:00 -0500

    removed Arduino Added Encoder/Pot

--- src/com/taurus/SwerveWheel.java
+++ src/com/taurus/SwerveWheel.java
@@ -5,23 +5,23 @@
  */
 
 package com.taurus;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.PIDOutput;
 
 /**
  *
  * @author Taurus Robotics
- * handle motor outputs and feedback for an individual wheel
+ handle motor outputs and feedback for an individual wheel
  */
 public class SwerveWheel
 {
     private SwervePoint WheelPosition;     // wheel location from center of robot
-    private SwervePoint WheelVelocity;     // wheel speed, x and y vals, hypotenuse val, angle
+    private SwervePoint WheelDesired;     // wheel speed, x and y vals, hypotenuse val, angle
+    private SwervePoint WheelActual;     // wheel speed, x and y vals, hypotenuse val, angle
     private Victor MotorDrive;
     private Victor MotorAngle;
-    private SwerveArduino Arduino;
     
     public double AngleP = 1;
     public double AngleI = 0;
@@ -30,33 +30,41 @@ public class SwerveWheel
     public double DriveI = 0;
     public double DriveD = 0;
     
-    private SwervePIDSource AngleSource;
+    private double DriveWheelDiameter = 4.0; 
+    private int DriveEncoderPulses = 64;
+    private double DriveEncoderRate = Math.PI*DriveWheelDiameter/DriveEncoderPulses;
+    
+    //private SwervePIDSource AngleSource;
+    private AnalogPotentiometer AnglePot;
     private PIDController AnglePID;
     
-    private SwervePIDSource DriveSource;
+    //private SwervePIDSource DriveSource;
+    private Encoder DriveEncoder;
     private PIDController DrivePID;
     // constructor
     // x and y are location relative to robot center
     // Address is slave address of arduino
-    public SwerveWheel(double x, double y, int Address, int Drive, int Angle)
+    public SwerveWheel(double x, double y, int EncoderA, int EncoderB, int Drive, int Angle, int Pot)
     {
         WheelPosition = new SwervePoint(x, y);
-        WheelVelocity = new SwervePoint(0, 0);
+        WheelActual = new SwervePoint(0, 0);
+        WheelDesired = new SwervePoint(0, 0);
         MotorDrive = new Victor(Drive);
         MotorAngle = new Victor(Angle);
-        Arduino = new SwerveArduino(Address);
         
-        //TODO need to implement this
-        AngleSource = new SwervePIDSource();
-        DriveSource = new SwervePIDSource();
+        DriveEncoder = new Encoder(EncoderA, EncoderB);
+        AnglePot = new AnalogPotentiometer(Pot);
         
-        AnglePID = new PIDController(AngleP, AngleI, AngleD, AngleSource, MotorAngle);
+        DriveEncoder.setDistancePerPulse(DriveEncoderRate);
+       
+        
+        AnglePID = new PIDController(AngleP, AngleI, AngleD, AnglePot, MotorAngle);
         AnglePID.setContinuous();
         AnglePID.setInputRange(0, 360);
         //Maybe set outputRange Depending on Victors 
         AnglePID.enable();
         
-        DrivePID = new PIDController(DriveP, DriveI, DriveD, DriveSource, MotorDrive);
+        DrivePID = new PIDController(DriveP, DriveI, DriveD, DriveEncoder, MotorDrive);
         DrivePID.setInputRange(-1, 1);
         //MAybe set output range depending on stuff
         DrivePID.enable();
@@ -65,44 +73,39 @@ public class SwerveWheel
     // set the velocity and desired rotation of the wheel using the whole robot's desired values
     // auto calculates what is needed for this specific wheel instance
     // return: actual reading from wheel
-    public SwervePoint Set(SwervePoint RobotVelocity, double RobotRotation)
+    public void Set(SwervePoint RobotVelocity, double RobotRotation)
     {
-        WheelVelocity = new SwervePoint(RobotVelocity.X() - RobotRotation * WheelPosition.Y(),
+        WheelDesired = new SwervePoint(RobotVelocity.X() - RobotRotation * WheelPosition.Y(),
                                         RobotVelocity.Y() + RobotRotation * WheelPosition.X());
  
-        return UpdateTask();
+        UpdateTask();
     }
  
     // get the desired/requested velocity and rotation of this wheel instance
     public SwervePoint GetDesired()
     {
-        return WheelVelocity;
+        return WheelDesired;
     }
  
-    // get the actual velocity and rotation of this wheel instance
-    // requires the UpdateTask to be called prior
     public SwervePoint GetActual()
     {
-        return Arduino.Get();
+        return WheelActual;
     }
- 
+
+    
     // Manually invoke updating the actual values and the motor outputs
     // called automatically from Set()
     // return: actual reading from wheel
-    public SwervePoint UpdateTask()
+    public void UpdateTask()
     {
-        Arduino.Update();
- 
+
         // handle motor outputs relative to the new readings
         // PID control here
-        AngleSource.pidSet(Arduino.Get().Angle());
         AnglePID.setPID(AngleP, AngleI, AngleD);
-        AnglePID.setSetpoint(WheelVelocity.Angle());
+        AnglePID.setSetpoint(WheelDesired.Angle());
         
-        DriveSource.pidSet(Arduino.Get().H());
         DrivePID.setPID(DriveP, DriveI, DriveD);
-        DrivePID.setSetpoint(WheelVelocity.H());
+        DrivePID.setSetpoint(WheelDesired.H());
         
-        return Arduino.Get();
     }
 }
