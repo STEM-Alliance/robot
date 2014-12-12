987850f3d8375d7261ebaf2445c94b42117e3a8a
Samuel Harrington <samuel.l.harrington@gmail.com>
2014-12-11 22:54:32 -0600

    Minor code simplification in SwerveAngleController

--- src/com/taurus/Utilities.java
+++ src/com/taurus/Utilities.java
@@ -1,23 +1,32 @@
 package com.taurus;
 
-public class Utilities
+public final class Utilities
 {
-    public static double copySign(double magnitude, double sign)
+    public static double wrapToRange(double value, double min, double max)
     {
-        if ((sign < 0) != (magnitude < 0))
-            return -magnitude;
-        return magnitude;
+        value -= min;
+        value %= max - min;
+        value += max - min;
+        value %= max - min;
+        value += min;
+        return value;
     }
-    
+
     public static double clampToRange(double value, double min, double max)
     {
         return value < min ? min
              : value > max ? max
                            : value;
     }
-    
-    public static double scaleToRange(double value, double inMin, double inMax, double outMin, double outMax)
+
+    public static double scaleToRange(
+            double value,
+            double inMin, double inMax,
+            double outMin, double outMax)
     {
-        return value * (outMax - outMin) / (inMax - inMin) + outMin;
+        return clampToRange(value, inMin, inMax)
+                * (outMax - outMin)
+                / (inMax - inMin)
+                + outMin;
     }
 }
--- utsrc/com/taurus/test/SwerveAngleControllerTest.java
+++ utsrc/com/taurus/test/SwerveAngleControllerTest.java
@@ -12,31 +12,34 @@ public class SwerveAngleControllerTest extends TestCase
         SwerveAngleController controller = new SwerveAngleController();
         assertEquals(0, controller.CalcErrorAndReverseNeeded(0, 0), 0.01);
         assertFalse(controller.isReverseDriveMotor());
-        
+
         assertEquals(0, controller.CalcErrorAndReverseNeeded(180, 0), 0.01);
         assertTrue(controller.isReverseDriveMotor());
-        
+
         assertEquals(0, controller.CalcErrorAndReverseNeeded(-180, 180), 0.01);
         assertFalse(controller.isReverseDriveMotor());
-        
+
         assertEquals(-50, controller.CalcErrorAndReverseNeeded(-30, 20), 0.01);
         assertFalse(controller.isReverseDriveMotor());
-        
+
+        assertEquals(-50, controller.CalcErrorAndReverseNeeded(-390, 380), 0.01);
+        assertFalse(controller.isReverseDriveMotor());
+
         assertEquals(50, controller.CalcErrorAndReverseNeeded(30, -20), 0.01);
         assertFalse(controller.isReverseDriveMotor());
-        
+
         assertEquals(-10, controller.CalcErrorAndReverseNeeded(-95, -85), 0.01);
         assertFalse(controller.isReverseDriveMotor());
-        
+
         assertEquals(-85, controller.CalcErrorAndReverseNeeded(0, -95), 0.01);
         assertTrue(controller.isReverseDriveMotor());
-        
+
         assertEquals(85, controller.CalcErrorAndReverseNeeded(0, 95), 0.01);
         assertTrue(controller.isReverseDriveMotor());
-        
+
         assertEquals(-5, controller.CalcErrorAndReverseNeeded(0, -175), 0.01);
         assertTrue(controller.isReverseDriveMotor());
-        
+
         assertEquals(-20, controller.CalcErrorAndReverseNeeded(-10, -170), 0.01);
         assertTrue(controller.isReverseDriveMotor());
     }
