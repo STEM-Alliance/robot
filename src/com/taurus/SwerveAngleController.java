5dedacd7106b9a34588d12b9409ec69d4f4db246
Samuel Harrington <samuel.l.harrington@gmail.com>
2014-12-12 01:23:25 -0600

    Switch SwerveAngleController to use the PIController class.

--- src/com/taurus/SwerveAngleController.java
+++ src/com/taurus/SwerveAngleController.java
@@ -5,19 +5,18 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public final class SwerveAngleController
 {
-    private static final double MaxTimestampDiff = 1.0;  // heuristic to detect disabling
     private static final double HalfCircle = 180, QuarterCircle = 90;
     private static final double MaxOut = 1;
     
     private static final double P = MaxOut / QuarterCircle * 0.01;
-    private static final double T_I = .5;  // seconds needed to equal a P term contribution
+    //private static final double T_I = .5;  // seconds needed to equal a P term contribution
     private static final double I = 0;//1 / T_I;
 
-    private String Name;
-    private double DriveMotorSpeed;
-    private boolean ReverseDriveMotor;
-    private double Integral;
-    private double LastTimestamp = 0;
+    private final String name;
+    private final PIController controller;
+    
+    private double driveMotorSpeed;
+    private boolean reverseDriveMotor;
 
     public SwerveAngleController()
     {
@@ -26,76 +25,36 @@ public final class SwerveAngleController
     
     public SwerveAngleController(String name)
     {
-        this.Name = name;
-        this.LastTimestamp = 0;
-        this.Reset();
-    }
-    
-    public void Reset()
-    {
-        this.DriveMotorSpeed = 0;
-        this.ReverseDriveMotor = false;
-        this.Integral = 0;
+        this.name = name;
+        this.driveMotorSpeed = 0;
+        this.reverseDriveMotor = false;
+        this.controller = new PIController(P, I, MaxOut);
     }
 
     public double getAngleMotorSpeed()
     {
-        return DriveMotorSpeed;
+        return driveMotorSpeed;
     }
 
     public boolean isReverseDriveMotor()
     {
-        return ReverseDriveMotor;
+        return reverseDriveMotor;
     }
 
     public void Update(double setPoint, double sensorValue)
     {
-        SmartDashboard.putNumber(Name + ".SetPoint", setPoint);
-        SmartDashboard.putNumber(Name + ".SensorValue", sensorValue);
+        SmartDashboard.putNumber(name + ".SetPoint", setPoint);
+        SmartDashboard.putNumber(name + ".SensorValue", sensorValue);
 
         // Calculate error, with detection of the drive motor reversal shortcut.
         double error = CalcErrorAndReverseNeeded(setPoint, sensorValue);
         
-        SmartDashboard.putNumber(Name + ".Error", error);
-        SmartDashboard.putBoolean(Name + ".Reverse", ReverseDriveMotor);
+        SmartDashboard.putNumber(name + ".Error", error);
+        SmartDashboard.putBoolean(name + ".Reverse", reverseDriveMotor);
 
-        // Proportional term.
-        double proportional = error * P;
+        driveMotorSpeed = this.controller.update(error, Timer.getFPGATimestamp());
         
-        SmartDashboard.putNumber(Name + ".Proportional", proportional);
-        
-        // Get time since we were last called.
-        double timeDelta;
-        {
-            double timestamp = Timer.getFPGATimestamp();
-            timeDelta = timestamp - LastTimestamp;
-            LastTimestamp = timestamp;
-        }
-        
-        // Calculate integral term.
-        if (timeDelta > MaxTimestampDiff)
-        {
-            // We were probably disabled; reset the integral error.
-            Integral = 0;
-        }
-        else
-        {
-            // Sum the proportional term over time.
-            Integral += proportional * timeDelta * I;
-            Integral = Utilities.clampToRange(Integral, -MaxOut, MaxOut);
-        }
-        
-        SmartDashboard.putNumber(Name + ".IntegratedError", Integral);
-
-        // Calculate output with coefficients.
-        double output = proportional + Integral;
-        
-        SmartDashboard.putNumber(Name + ".Output", output);
-
-        // Clamp output.
-        DriveMotorSpeed = Utilities.clampToRange(output, -MaxOut, MaxOut);
-        
-        SmartDashboard.putNumber(Name + ".ClampedOutput", DriveMotorSpeed);
+        SmartDashboard.putNumber(name + ".Output", driveMotorSpeed);
     }
 
     public double CalcErrorAndReverseNeeded(double setPoint, double sensorValue)
@@ -108,8 +67,8 @@ public final class SwerveAngleController
         double reversedMotorError = Utilities.wrapToRange(error + HalfCircle, -HalfCircle, HalfCircle);
         
         // Pick the smaller error.
-        ReverseDriveMotor = Math.abs(reversedMotorError) < Math.abs(error);
-        if (ReverseDriveMotor)
+        reverseDriveMotor = Math.abs(reversedMotorError) < Math.abs(error);
+        if (reverseDriveMotor)
             error = reversedMotorError;
         
         return error;
