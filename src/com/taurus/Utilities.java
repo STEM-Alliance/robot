e37230b16be47de4688b7a3ff8dd580766ba246c
Samuel Harrington <samuel.l.harrington@gmail.com>
2014-12-22 01:55:52 -0600

    Improve the readability of the Utilities by splitting some methods
    Also improve the comments

--- src/com/taurus/Utilities.java
+++ src/com/taurus/Utilities.java
@@ -8,21 +8,31 @@ package com.taurus;
 public final class Utilities
 {
     /**
-     * Ensure the value is between min/max, and if it is outside the range,
+     * Ensure the value is between min and max, and if it is outside the range,
      * wrap it around.
      * @param value Input value
      * @param min Minimum expected output value
      * @param max Maximum expected output value
-     * @return
+     * @return The value wrapped to between min and max
      */
     public static final double wrapToRange(double value, double min, double max)
     {
-        value -= min;
-        value %= max - min;
-        value += max - min;
-        value %= max - min;
-        value += min;
-        return value;
+        // Subract off the min, wrap to the range, and add the min again
+        return wrapToRange(value - min, max - min) + min;
+    }
+    
+    /**
+     * Ensure the value is between 0 and max, and if it is outside the range,
+     * wrap it around.
+     * @param value Input value
+     * @param max Maximum expected output value
+     * @return The value wrapped to between 0 and max
+     */
+    public static final double wrapToRange(double value, double max)
+    {
+        // The Java mod operator returns values from -max to max,
+        // so we need to add max and mod again
+        return ((value % max) + max) % max;
     }
 
     /**
@@ -30,15 +40,27 @@ public final class Utilities
      * @param value Input value
      * @param min Minimum expected output value
      * @param max Maximum expected output value
-     * @return
+     * @return A value between min and max
      */
     public static final double clampToRange(double value, double min, double max)
     {
-        return value < min ? min
-             : value > max ? max
-                           : value;
+        if (value > min)
+        {
+            if (value < max)
+            {
+                return value;
+            }
+            else
+            {
+                return max;
+            }
+        }
+        else
+        {
+            return min;
+        }
     }
-
+    
     /**
      * Scale a value from the expected input range to the expected output range.
      * @param value Input value
@@ -46,16 +68,30 @@ public final class Utilities
      * @param inMax Maximum input value
      * @param outMin Minimum expected output value
      * @param outMax Maximum expected output value
-     * @return
+     * @return A value between outMin and outMax
      */
     public static final double scaleToRange(
             double value,
             double inMin, double inMax,
             double outMin, double outMax)
     {
-        return (clampToRange(value, inMin, inMax) - inMin)
-                * (outMax - outMin)
-                / (inMax - inMin)
+        // Subtract off the input min value, 
+        // scale based on the in and out ranges, 
+        // and add the output min value
+        return scaleToRange(value - inMin, inMax - inMin, outMax - outMin)
                 + outMin;
+    }
+
+    /**
+     * Scale a value from the expected input range to the expected output range.
+     * @param value Input value
+     * @param inMax Maximum input value
+     * @param outMax Maximum expected output value
+     * @return A value between 0 and outMax
+     */
+    public static final double scaleToRange(
+            double value, double inMax, double outMax)
+    {
+        return clampToRange(value, 0, inMax) * outMax / inMax;
     }
 }
