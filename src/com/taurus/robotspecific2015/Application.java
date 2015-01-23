ac78692afad17505f93ead8d86301a62ab365ef1
David Lindner <lindnerdavidr@johndeere.com>
2015-01-22 23:36:58 -0600

    Adding application interface. Enables autonomous development.

---
+++ src/com/taurus/robotspecific2015/Application.java
@@ -0,0 +1,43 @@
+package com.taurus.robotspecific2015;
+
+import com.taurus.IApplication;
+import com.taurus.SwerveApplication;
+
+public class Application extends SwerveApplication
+{
+    // Runs when robot is disabled
+    public void disabledPeriodic()
+    {
+    	
+    }
+    
+    // Runs when robot is disabled
+    public void disabledInit()
+    {
+    	
+    }
+    
+    // Runs when operator mode is enabled.
+    public void teleopInit()
+    {
+    	super.teleopInit(); // Do not remove
+    }
+
+    // Runs during operator control
+    public void teleopPeriodic()
+    {
+        super.teleopPeriodic();	// Do not remove
+    }
+    
+    // Called at start of autonomous mode
+    public void autonomousInit()
+    {
+    	
+    }
+
+    // Called periodically during autonomous mode
+    public void autonomousPeriodic()
+    {
+    
+    }
+}
