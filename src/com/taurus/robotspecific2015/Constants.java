70c37a0b9e041c43c191b39994e09fc8ac68a6ca
David Lindner <lindnerdavidr@johndeere.com>
2015-01-22 21:11:28 -0600

    Abstracting pneumatics

---
+++ src/com/taurus/robotspecific2015/Constants.java
@@ -0,0 +1,12 @@
+package com.taurus.robotspecific2015;
+
+public abstract class Constants 
+{
+   // Pneumatics
+   public static final int PCUModuleID = 9;
+   public static final int ChannelRail = 0;
+   public static final int ChannelContainerCar = 1;
+   public static final int ChannelContainerFixed = 2;
+   public static final int ChannelStackHolder = 3;
+   public static final int ChannelJawsOfLife = 4;   
+}
---
+++ src/com/taurus/robotspecific2015/Lift.java
@@ -0,0 +1,46 @@
+package RobotSpecific2015;
+
+// Manages manipulators and supporting systems
+public class Lift 
+{   
+   PneumaticSubsystem Rails;
+   PneumaticSubsystem ContainerCar;
+   PneumaticSubsystem ContainerFixed;
+   PneumaticSubsystem StackHolder;
+   PneumaticSubsystem JawsOfLife;
+   
+   // Initialize lift and all objects owned by the lift
+   public Lift()
+   {
+	   // Create all objects owned by this class
+	   Rails = new PneumaticSubsystem(Constants.ChannelRail, true);
+	   ContainerCar = new PneumaticSubsystem(Constants.ChannelContainerCar, false);
+	   ContainerFixed = new PneumaticSubsystem(Constants.ChannelContainerFixed, false);	   
+	   StackHolder = new PneumaticSubsystem(Constants.ChannelStackHolder, false);
+	   JawsOfLife = new PneumaticSubsystem(Constants.ChannelJawsOfLife, false);
+   }
+   
+   // Routine to add new tote to existing stack
+   public void AddToteToStack()
+   {
+	   // Assumption: We have sensed that the tote is already in the robot, ready to be lifted
+	   // Assumption: Pneumatics in expected starting position
+	   
+	   // TODO: Raise the car up until top position sensed
+	   
+	   // TODO: Extend the stack holder pneumatics to hold the totes up
+	   
+	   // TODO: Lower the car until bottom position sensed
+   }
+   
+   // Routine to lift container to start a new stack
+   public void AddContainerToStack()
+   {
+	   
+   }
+   
+   public void EjectStack()
+   {
+	   
+   }
+}
