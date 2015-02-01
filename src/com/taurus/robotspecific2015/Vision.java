51f88053616fec940af2c3e413ca15270177d510
Samuel Harrington <samuel.l.harrington@gmail.com>
2015-02-01 14:31:01 -0600

    Readd vision particle work after merge

--- src/com/taurus/robotspecific2015/Vision.java
+++ src/com/taurus/robotspecific2015/Vision.java
@@ -1,12 +1,7 @@
 package com.taurus.robotspecific2015;
 
 import com.ni.vision.NIVision;
-import com.ni.vision.NIVision.ColorMode;
-import com.ni.vision.NIVision.DrawMode;
-import com.ni.vision.NIVision.GetImageSizeResult;
-import com.ni.vision.NIVision.Image;
-import com.ni.vision.NIVision.Point;
-import com.ni.vision.NIVision.Range;
+import com.ni.vision.NIVision.*;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Timer;
@@ -28,7 +23,7 @@ public class Vision implements Runnable {
 //    private static final String ATTR_BR_VALUE = "CameraAttributes::Brightness::Value";
 
     private int session;
-    private Image frame;
+    private Image frame, frameWithLine, frameTH;
 
     Thread visionThread;
 
@@ -36,6 +31,8 @@ public class Vision implements Runnable {
     private int ToteLineStartY = 60 / 240;
     private int ToteLineEndX = 60 / 320;
     private int ToteLineEndY = 180 / 240;
+
+    private boolean toteDetectionOn = true;
 
     public Vision()
     {
@@ -58,6 +55,8 @@ public class Vision implements Runnable {
     public void run()
     {
         frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+        frameWithLine = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+        frameTH = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_U8, 0);
 
         // the camera name (ex "cam0") can be found through the roborio web
         // interface
@@ -116,57 +115,64 @@ public class Vision implements Runnable {
                 TimeLastVision = Timer.getFPGATimestamp();
 
                 NIVision.IMAQdxGrab(session, frame, 1);
-
-                chooser = ((Integer) imageChooser.getSelected()).intValue();
-
-                if (chooser == 0)
+                
+                if (((Integer)imageChooser.getSelected()).intValue() == 0)
                 {
                     CameraServer.getInstance().setImage(frame);
-                }
-                else if (chooser == 1)
-                {
-                    NIVision.imaqColorThreshold(frame, frame, 255,
-                            ColorMode.HSL,
-                            new Range(Application.prefs.getInt("Hmin", 30),
-                                    Application.prefs.getInt("Hmax", 60)),
-                            new Range(Application.prefs.getInt("Smin", 60),
-                                    Application.prefs.getInt("Smax", 255)),
-                            new Range(Application.prefs.getInt("Lmin", 100),
-                                    Application.prefs.getInt("Lmax", 255)));
-
-                    // TODO: NIVision.imaqParticleFilter4(frame, frame, new
-                    // Particle, options, roi)
-
-                    CameraServer.getInstance().setImage(frame);
-                }
-                else if (chooser == 2)
+                }                
+                else if (((Integer)imageChooser.getSelected()).intValue() == 1)
                 {
                     for (int i = 0; i < 4; i++)
                     {
-                        NIVision.imaqDrawLineOnImage(frame, frame,
+                        NIVision.imaqDrawLineOnImage(frameWithLine, frame,
                                 DrawMode.DRAW_VALUE, ToteLines[0][i],
                                 ToteLines[1][i], 0.0f);
-
                     }
-
-                    // for (int i = 0; i <= size.height; i += (size.height/12))
-                    // {
-                    // NIVision.imaqDrawLineOnImage(frame, frame,
-                    // DrawMode.DRAW_VALUE, new Point(0, i),
-                    // new Point(size.width, i), 0.0f);
-                    // }
-                    // for (int i = 0; i <= size.width; i += (size.width/16))
-                    // {
-                    // NIVision.imaqDrawLineOnImage(frame, frame,
-                    // DrawMode.DRAW_VALUE, new Point(i, 0),
-                    // new Point(i, size.height), 0.0f);
-                    // }
-
-                    CameraServer.getInstance().setImage(frame);
+                        
+                    CameraServer.getInstance().setImage(frameWithLine);
                 }
 
-                SmartDashboard.putNumber("Vision Task Length",
-                        Timer.getFPGATimestamp() - TimeLastVision);
+                if (toteDetectionOn)
+                {
+                    NIVision.imaqColorThreshold(frameTH, frame, 255, ColorMode.HSL, 
+                            new Range(Application.prefs.getInt("Hmin", 30), Application.prefs.getInt("Hmax", 60)), 
+                            new Range(Application.prefs.getInt("Smin", 60), Application.prefs.getInt("Smax", 255)), 
+                            new Range(Application.prefs.getInt("Lmin", 100), Application.prefs.getInt("Lmax", 255)));
+                    
+                    if (((Integer)imageChooser.getSelected()).intValue() == 2)
+                    {
+                        CameraServer.getInstance().setImage(frameTH);
+                    }
+                    
+                    ParticleFilterCriteria2[] criteria =
+                        new ParticleFilterCriteria2[] {
+                            new ParticleFilterCriteria2(
+                                    MeasurementType.MT_AREA_BY_IMAGE_AREA, 
+                                    /*lower*/ Application.prefs.getInt("AreaMin", 10), 
+                                    /*upper*/ Application.prefs.getInt("AreaMax", 76800), 
+                                    /*calibrated*/ 0, /*exclude*/ 0),
+                        };
+                    ParticleFilterOptions2 options = 
+                            new ParticleFilterOptions2(/*rejectMatches*/ 0, /*rejectBorder*/ 0, /*fillHoles*/ 0, /*connectivity8*/ 1);
+                    ROI roi = NIVision.imaqCreateROI();
+                    
+                    NIVision.imaqParticleFilter4(frameTH, frameTH, criteria, options, roi);
+                    
+                    int particleCount = NIVision.imaqCountParticles(frameTH, /*connectivity8*/ 1);
+                    SmartDashboard.putNumber("Particles", particleCount);
+                    
+                    for (int i = 0; i < particleCount; i++)
+                    {
+                        NIVision.imaqDrawShapeOnImage(frameTH, frameTH, new Rect(0,0,10,10), DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 255);
+                    }
+                    
+                    if (((Integer)imageChooser.getSelected()).intValue() == 3)
+                    {
+                        CameraServer.getInstance().setImage(frameTH);
+                    }
+                }
+                
+                SmartDashboard.putNumber("Vision Task Length", Timer.getFPGATimestamp() - TimeLastVision);
             }
             Timer.delay(Math.max(0,
                     TIME_RATE_VISION
