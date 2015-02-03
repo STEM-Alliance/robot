95c625a2d92f435d888c79ce2cc17b57206f5555
Samuel Harrington <samuel.l.harrington@gmail.com>
2015-02-02 19:46:01 -0600

    Return info from the vision thread and start of tote alignment

--- src/com/taurus/robotspecific2015/VisionApplication.java
+++ src/com/taurus/robotspecific2015/VisionApplication.java
@@ -1,6 +1,10 @@
 package com.taurus.robotspecific2015;
 
 import com.taurus.Application;
+import com.taurus.Utilities;
+import com.taurus.swerve.SwerveVector;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class VisionApplication extends Application {
 
@@ -44,7 +48,21 @@ public class VisionApplication extends Application {
     @Override
     public void AutonomousPeriodicRobotSpecific()
     {
-        // TODO Auto-generated method stub
+        double maxSlide = prefs.getDouble("MaxSlide", 1), 
+                slideP = prefs.getDouble("SlideP", 1), 
+                targetX = prefs.getDouble("SlideTargetX", .5), 
+                forwardSpeed =  prefs.getDouble("AutoSpeed", .5);
+        SwerveVector Velocity = new SwerveVector(
+                Utilities.clampToRange((vision.getResultX() - targetX) * slideP, -maxSlide, maxSlide), 
+                forwardSpeed);
+        
+        double Rotation = 0;
+        double Heading = 0;
+        
+        SmartDashboard.putNumber("Velocity X", Velocity.getX());
+        SmartDashboard.putNumber("Velocity Y", Velocity.getY());
+        
+        drive.UpdateDrive(Velocity, Rotation, Heading);
 
     }
 
