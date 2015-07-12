0795918820b4eed401db272d1711840a3857b09f
David Lindner <David Lindner@localhost>
2015-07-12 13:42:18 -0500

    merging swerve dependancies from branch "2015 Competition Robot"

--- src/com/taurus/Utilities.java
+++ src/com/taurus/Utilities.java
@@ -58,7 +58,6 @@ public final class Utilities {
         // so we need to add max and mod again
         return ((value % max) + max) % max;
     }
-
     /**
      * Ensure the value is between min and max, and if it is outside the range,
      * wrap it around.
@@ -129,7 +128,7 @@ public final class Utilities {
         // scale based on the in and out ranges,
         // and add the output min value
         return scaleToRange(value - inMin, inMax - inMin, outMax - outMin)
-               + outMin;
+                + outMin;
     }
 
     /**
@@ -148,10 +147,9 @@ public final class Utilities {
     {
         return clampToRange(value, 0, inMax) * outMax / inMax;
     }
-
+    
     /**
      * Find the closest position given the input
-     * 
      * @param Input
      * @param Positions
      * @return
@@ -165,8 +163,7 @@ public final class Utilities {
         {
             final double diff = Math.abs(Positions[i] - Input);
 
-            if (diff < min)
-            {
+            if (diff < min) {
                 min = diff;
                 closest = Positions[i];
             }
@@ -174,98 +171,92 @@ public final class Utilities {
 
         return closest;
     }
-
+    
     /**
      * Check if a number is between two others
-     * 
      * @param val
      * @param min
      * @param max
      * @return
      */
-    public static boolean isBetween(double val, double min, double max)
-    {
+    public static boolean isBetween(double val, double min, double max) {
         return max > min ? val > min && val < max : val > max && val < min;
     }
+    
 
     /**
-     * Returns the largest (closest to positive infinity) {@code int} value that
-     * is less than or equal to the algebraic quotient. There is one special
-     * case, if the dividend is the {@linkplain Integer#MIN_VALUE
-     * Integer.MIN_VALUE} and the divisor is {@code -1}, then integer overflow
-     * occurs and the result is equal to the {@code Integer.MIN_VALUE}.
+     * Returns the largest (closest to positive infinity)
+     * {@code int} value that is less than or equal to the algebraic quotient.
+     * There is one special case, if the dividend is the
+     * {@linkplain Integer#MIN_VALUE Integer.MIN_VALUE} and the divisor is {@code -1},
+     * then integer overflow occurs and
+     * the result is equal to the {@code Integer.MIN_VALUE}.
      * <p>
      * Normal integer division operates under the round to zero rounding mode
-     * (truncation). This operation instead acts under the round toward negative
-     * infinity (floor) rounding mode. The floor rounding mode gives different
-     * results than truncation when the exact result is negative.
+     * (truncation).  This operation instead acts under the round toward
+     * negative infinity (floor) rounding mode.
+     * The floor rounding mode gives different results than truncation
+     * when the exact result is negative.
      * <ul>
-     * <li>If the signs of the arguments are the same, the results of
-     * {@code floorDiv} and the {@code /} operator are the same. <br>
-     * For example, {@code floorDiv(4, 3) == 1} and {@code (4 / 3) == 1}.</li>
-     * <li>If the signs of the arguments are different, the quotient is negative
-     * and {@code floorDiv} returns the integer less than or equal to the
-     * quotient and the {@code /} operator returns the integer closest to zero.<br>
-     * For example, {@code floorDiv(-4, 3) == -2}, whereas
-     * {@code (-4 / 3) == -1}.</li>
+     *   <li>If the signs of the arguments are the same, the results of
+     *       {@code floorDiv} and the {@code /} operator are the same.  <br>
+     *       For example, {@code floorDiv(4, 3) == 1} and {@code (4 / 3) == 1}.</li>
+     *   <li>If the signs of the arguments are different,  the quotient is negative and
+     *       {@code floorDiv} returns the integer less than or equal to the quotient
+     *       and the {@code /} operator returns the integer closest to zero.<br>
+     *       For example, {@code floorDiv(-4, 3) == -2},
+     *       whereas {@code (-4 / 3) == -1}.
+     *   </li>
      * </ul>
      * <p>
      *
-     * @param x
-     *            the dividend
-     * @param y
-     *            the divisor
-     * @return the largest (closest to positive infinity) {@code int} value that
-     *         is less than or equal to the algebraic quotient.
-     * @throws ArithmeticException
-     *             if the divisor {@code y} is zero
+     * @param x the dividend
+     * @param y the divisor
+     * @return the largest (closest to positive infinity)
+     * {@code int} value that is less than or equal to the algebraic quotient.
+     * @throws ArithmeticException if the divisor {@code y} is zero
      * @see #floorMod(int, int)
      * @see #floor(double)
      * @since 1.8
      */
-    public static int floorDiv(int x, int y)
-    {
+    public static int floorDiv(int x, int y) {
         int r = x / y;
         // if the signs are different and modulo not zero, round down
-        if ((x ^ y) < 0 && (r * y != x))
-        {
+        if ((x ^ y) < 0 && (r * y != x)) {
             r--;
         }
         return r;
     }
 
     /**
-     * Returns the largest (closest to positive infinity) {@code long} value
-     * that is less than or equal to the algebraic quotient. There is one
-     * special case, if the dividend is the {@linkplain Long#MIN_VALUE
-     * Long.MIN_VALUE} and the divisor is {@code -1}, then integer overflow
-     * occurs and the result is equal to the {@code Long.MIN_VALUE}.
+     * Returns the largest (closest to positive infinity)
+     * {@code long} value that is less than or equal to the algebraic quotient.
+     * There is one special case, if the dividend is the
+     * {@linkplain Long#MIN_VALUE Long.MIN_VALUE} and the divisor is {@code -1},
+     * then integer overflow occurs and
+     * the result is equal to the {@code Long.MIN_VALUE}.
      * <p>
      * Normal integer division operates under the round to zero rounding mode
-     * (truncation). This operation instead acts under the round toward negative
-     * infinity (floor) rounding mode. The floor rounding mode gives different
-     * results than truncation when the exact result is negative.
+     * (truncation).  This operation instead acts under the round toward
+     * negative infinity (floor) rounding mode.
+     * The floor rounding mode gives different results than truncation
+     * when the exact result is negative.
      * <p>
      * For examples, see {@link #floorDiv(int, int)}.
      *
-     * @param x
-     *            the dividend
-     * @param y
-     *            the divisor
-     * @return the largest (closest to positive infinity) {@code long} value
-     *         that is less than or equal to the algebraic quotient.
-     * @throws ArithmeticException
-     *             if the divisor {@code y} is zero
+     * @param x the dividend
+     * @param y the divisor
+     * @return the largest (closest to positive infinity)
+     * {@code long} value that is less than or equal to the algebraic quotient.
+     * @throws ArithmeticException if the divisor {@code y} is zero
      * @see #floorMod(long, long)
      * @see #floor(double)
      * @since 1.8
      */
-    public static long floorDiv(long x, long y)
-    {
+    public static long floorDiv(long x, long y) {
         long r = x / y;
         // if the signs are different and modulo not zero, round down
-        if ((x ^ y) < 0 && (r * y != x))
-        {
+        if ((x ^ y) < 0 && (r * y != x)) {
             r--;
         }
         return r;
@@ -274,54 +265,48 @@ public final class Utilities {
     /**
      * Returns the floor modulus of the {@code int} arguments.
      * <p>
-     * The floor modulus is {@code x - (floorDiv(x, y) * y)}, has the same sign
-     * as the divisor {@code y}, and is in the range of
-     * {@code -abs(y) < r < +abs(y)}.
+     * The floor modulus is {@code x - (floorDiv(x, y) * y)},
+     * has the same sign as the divisor {@code y}, and
+     * is in the range of {@code -abs(y) < r < +abs(y)}.
      *
      * <p>
-     * The relationship between {@code floorDiv} and {@code floorMod} is such
-     * that:
+     * The relationship between {@code floorDiv} and {@code floorMod} is such that:
      * <ul>
-     * <li>{@code floorDiv(x, y) * y + floorMod(x, y) == x}
+     *   <li>{@code floorDiv(x, y) * y + floorMod(x, y) == x}
      * </ul>
      * <p>
-     * The difference in values between {@code floorMod} and the {@code %}
-     * operator is due to the difference between {@code floorDiv} that returns
-     * the integer less than or equal to the quotient and the {@code /} operator
-     * that returns the integer closest to zero.
+     * The difference in values between {@code floorMod} and
+     * the {@code %} operator is due to the difference between
+     * {@code floorDiv} that returns the integer less than or equal to the quotient
+     * and the {@code /} operator that returns the integer closest to zero.
      * <p>
      * Examples:
      * <ul>
-     * <li>If the signs of the arguments are the same, the results of
-     * {@code floorMod} and the {@code %} operator are the same. <br>
-     * <ul>
-     * <li>{@code floorMod(4, 3) == 1}; &nbsp; and {@code (4 % 3) == 1}</li>
-     * </ul>
-     * <li>If the signs of the arguments are different, the results differ from
-     * the {@code %} operator.<br>
-     * <ul>
-     * <li>{@code floorMod(+4, -3) == -2}; &nbsp; and {@code (+4 % -3) == +1}</li>
-     * <li>{@code floorMod(-4, +3) == +2}; &nbsp; and {@code (-4 % +3) == -1}</li>
-     * <li>{@code floorMod(-4, -3) == -1}; &nbsp; and {@code (-4 % -3) == -1 }</li>
-     * </ul>
-     * </li>
+     *   <li>If the signs of the arguments are the same, the results
+     *       of {@code floorMod} and the {@code %} operator are the same.  <br>
+     *       <ul>
+     *       <li>{@code floorMod(4, 3) == 1}; &nbsp; and {@code (4 % 3) == 1}</li>
+     *       </ul>
+     *   <li>If the signs of the arguments are different, the results differ from the {@code %} operator.<br>
+     *      <ul>
+     *      <li>{@code floorMod(+4, -3) == -2}; &nbsp; and {@code (+4 % -3) == +1} </li>
+     *      <li>{@code floorMod(-4, +3) == +2}; &nbsp; and {@code (-4 % +3) == -1} </li>
+     *      <li>{@code floorMod(-4, -3) == -1}; &nbsp; and {@code (-4 % -3) == -1 } </li>
+     *      </ul>
+     *   </li>
      * </ul>
      * <p>
-     * If the signs of arguments are unknown and a positive modulus is needed it
-     * can be computed as {@code (floorMod(x, y) + abs(y)) % abs(y)}.
+     * If the signs of arguments are unknown and a positive modulus
+     * is needed it can be computed as {@code (floorMod(x, y) + abs(y)) % abs(y)}.
      *
-     * @param x
-     *            the dividend
-     * @param y
-     *            the divisor
+     * @param x the dividend
+     * @param y the divisor
      * @return the floor modulus {@code x - (floorDiv(x, y) * y)}
-     * @throws ArithmeticException
-     *             if the divisor {@code y} is zero
+     * @throws ArithmeticException if the divisor {@code y} is zero
      * @see #floorDiv(int, int)
      * @since 1.8
      */
-    public static int floorMod(int x, int y)
-    {
+    public static int floorMod(int x, int y) {
         int r = x - floorDiv(x, y) * y;
         return r;
     }
@@ -329,163 +314,44 @@ public final class Utilities {
     /**
      * Returns the floor modulus of the {@code long} arguments.
      * <p>
-     * The floor modulus is {@code x - (floorDiv(x, y) * y)}, has the same sign
-     * as the divisor {@code y}, and is in the range of
-     * {@code -abs(y) < r < +abs(y)}.
+     * The floor modulus is {@code x - (floorDiv(x, y) * y)},
+     * has the same sign as the divisor {@code y}, and
+     * is in the range of {@code -abs(y) < r < +abs(y)}.
      *
      * <p>
-     * The relationship between {@code floorDiv} and {@code floorMod} is such
-     * that:
+     * The relationship between {@code floorDiv} and {@code floorMod} is such that:
      * <ul>
-     * <li>{@code floorDiv(x, y) * y + floorMod(x, y) == x}
+     *   <li>{@code floorDiv(x, y) * y + floorMod(x, y) == x}
      * </ul>
      * <p>
      * For examples, see {@link #floorMod(int, int)}.
      *
-     * @param x
-     *            the dividend
-     * @param y
-     *            the divisor
+     * @param x the dividend
+     * @param y the divisor
      * @return the floor modulus {@code x - (floorDiv(x, y) * y)}
-     * @throws ArithmeticException
-     *             if the divisor {@code y} is zero
+     * @throws ArithmeticException if the divisor {@code y} is zero
      * @see #floorDiv(long, long)
      * @since 1.8
      */
-    public static long floorMod(long x, long y)
-    {
+    public static long floorMod(long x, long y) {
         return x - floorDiv(x, y) * y;
     }
 
     /**
-     * Returns the absolute value of an {@code int} value. If the argument is
-     * not negative, the argument is returned. If the argument is negative, the
-     * negation of the argument is returned.
+     * Returns the absolute value of an {@code int} value.
+     * If the argument is not negative, the argument is returned.
+     * If the argument is negative, the negation of the argument is returned.
      *
-     * <p>
-     * Note that if the argument is equal to the value of
-     * {@link Integer#MIN_VALUE}, the most negative representable {@code int}
-     * value, the result is that same value, which is negative.
+     * <p>Note that if the argument is equal to the value of
+     * {@link Integer#MIN_VALUE}, the most negative representable
+     * {@code int} value, the result is that same value, which is
+     * negative.
      *
-     * @param a
-     *            the argument whose absolute value is to be determined
-     * @return the absolute value of the argument.
+     * @param   a   the argument whose absolute value is to be determined
+     * @return  the absolute value of the argument.
      */
-    public static int abs(int a)
-    {
+    public static int abs(int a) {
         return (a < 0) ? -a : a;
     }
-
-    /**
-     * Round a value to the desired number of decimal places
-     * 
-     * @param d
-     *            value to round
-     * @param res
-     *            number of decimal places
-     * @return
-     */
-    public static double round(double d, int res)
-    {
-        int x = (int) Math.pow(10, res);
-        return Math.rint(d * x) / x;
-    }
-
-    /**
-     * Split up a string into an array, delimited by spaces
-     * 
-     * @param string
-     * @param wordsPerLine
-     * @return
-     */
-    public static String[] splitStringByWords(String string, int wordsPerLine)
-    {
-        String[] strings = string.split(" ");
-        int last = strings.length % wordsPerLine;
-        int keys = strings.length / wordsPerLine;
-        if (last != 0)
-        {
-            keys += 1;
-        }
-
-        String[] lines = new String[keys];
-
-        for (int i = 0; i < keys; i++)
-        {
-            int iteration = wordsPerLine;
-            if (i == keys - 1 && last != 0)
-            {
-                iteration = last;
-            }
-
-            int offset = i * wordsPerLine;
-            String line = "";
-
-            for (int j = offset; j < offset + iteration; j++)
-            {
-                line += strings[j] + " ";
-            }
-
-            lines[i] = line;
-        }
-        return lines;
-    }
-
-    /**
-     * Perform a deep copy of a three dimensional array. Very inefficient
-     * 
-     * @param arr
-     * @return
-     */
-    public static double[][][] arrayCopy(double[][][] arr)
-    {
-        // size first dimension of array
-        double[][][] temp = new double[arr.length][][];
-
-        for (int i = 0; i < arr.length; i++)
-        {
-            // first dimension of array
-            temp[i] = arrayCopy(arr[i]);
-        }
-
-        return temp;
-    }
-
-    /**
-     * Perform a deep copy of a two dimensional array. Very inefficient
-     * 
-     * @param arr
-     * @return
-     */
-    public static double[][] arrayCopy(double[][] arr)
-    {
-        // size first dimension of array
-        double[][] temp = new double[arr.length][arr[0].length];
-
-        for (int i = 0; i < arr.length; i++)
-        {
-            // first dimension of array
-            temp[i] = arrayCopy(arr[i]);
-        }
-
-        return temp;
-    }
-
-    /**
-     * Perform a deep copy of a one dimensional array. Very inefficient
-     * 
-     * @param arr
-     * @return
-     */
-    public static double[] arrayCopy(double[] arr)
-    {
-        double[] temp = new double[arr.length];
-
-        for (int i = 0; i < arr.length; i++)
-        {
-            temp[i] = arr[i];
-        }
-
-        return temp;
-    }
+    
 }
