450879117b20db4ca4b9c61dc388ba4eb188094e
Brad Poppe <poppeseed@gmail.com>
2016-01-31 18:54:21 -0600

    Cleaning up the lift commands & RockerDrive.

--- src/com/taurus/commands/LiftLower.java
+++ src/com/taurus/commands/LiftLower.java
@@ -5,19 +5,22 @@ import com.taurus.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class LiftLower extends Command {
-private boolean done;
-private double endHeight;
+    private boolean done;
+    private double endHeight;
+    
     public LiftLower() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         requires(Robot.liftSubsystem);
     }
+    
     // Called just before this Command runs the first time
     protected void initialize() {
         done = false;
         double heightIncrement = 25.4;//mm = 1 in
         endHeight = (Robot.liftSubsystem.getHeight() - heightIncrement);
     }
+    
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         done = Robot.liftSubsystem.setHeight(endHeight);//store result of setHeight function into the done variable
@@ -27,9 +30,11 @@ private double endHeight;
     protected boolean isFinished() {
         return done;
     }
+    
     // Called once after isFinished returns true
     protected void end() {
     }
+    
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
