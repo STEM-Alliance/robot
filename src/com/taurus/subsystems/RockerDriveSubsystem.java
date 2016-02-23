efd02686129b9badb7cda55c2e682ffe419c6f8a
FRC 4818 The Herd <wfrobotics@gmail.com>
2016-02-23 16:14:28 -0600

    Fixing drive raw adjustments.

--- src/com/taurus/subsystems/RockerDriveSubsystem.java
+++ src/com/taurus/subsystems/RockerDriveSubsystem.java
@@ -126,18 +126,11 @@ public class RockerDriveSubsystem extends Subsystem
      */
     public void driveRaw(double right, double left, boolean tractionControlEnabled)
     {
-        int rThing;
-        int lThing; 
-        if (right< 0)
-            rThing =-1;
-        else 
-            rThing = 1;
-        if(left>0)     
-            lThing = -1;
-        else 
-            lThing = 1; 
-                
-        driveRaw(new double[]{right,(Math.abs(right)-.1)*rThing,right}, new double[]{left,(Math.abs(left)-.1)*lThing,left}, tractionControlEnabled);
+        double rThing = Math.signum(right);
+        double lThing = Math.signum(left);
+        driveRaw(new double[]{right, (Math.abs(right)-.1)*rThing, right}, 
+                 new double[]{left,  (Math.abs(left)-.1)*lThing,  left},
+                 tractionControlEnabled);
     }
     
     /**
