66c3202d7180e33b800e357df61106e889765ff2
Brad Poppe <poppeseed@gmail.com>
2016-03-02 21:20:03 -0600

    Cleaning up a bunch of commands.

--- src/com/taurus/commands/LiftMoveDistance.java
+++ src/com/taurus/commands/LiftMoveDistance.java
@@ -16,34 +16,28 @@ public class LiftMoveDistance extends Command {
      * @param distance (signed inches)
      */
     public LiftMoveDistance(double distance) {
-        // Use requires() here to declare subsystem dependencies
         requires(Robot.liftSubsystem);
         this.distance = distance;
     }
     
-    // Called just before this Command runs the first time
     protected void initialize() {
         done = false;
         endHeight = (Robot.liftSubsystem.getHeightFromFloorAverage() + distance);
     }
     
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         Utilities.PrintCommand("Lift", this);
         done = Robot.liftSubsystem.setHeightFromFloor(endHeight);//store result of setHeight function into the done variable
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return done;
     }
     
-    // Called once after isFinished returns true
     protected void end() {
     }
     
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
