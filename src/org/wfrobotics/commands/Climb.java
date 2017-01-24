6cf48542038d971f10fea2b2d8644b6baa633572
FRC 4818 The Herd <wfrobotics@gmail.com>
2017-01-24 16:44:28 -0600

    Missing climber update from yesterday

--- src/org/wfrobotics/commands/Climb.java
+++ src/org/wfrobotics/commands/Climb.java
@@ -9,6 +9,8 @@ public class Climb extends CommandGroup
     public Climb()
     {
         addSequential(new Up(MODE.CLIMB));
+        // TODO Flaunt. Flash lights when we are at the top of the rope.
+        addSequential(new Up(MODE.HOLD));
         // TODO DRL command to brake for after the robot is disable at the end of the match
     }
 }
