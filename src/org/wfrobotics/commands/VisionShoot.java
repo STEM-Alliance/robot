4acb3373b57516412c7d82b3a4408c7faed9c90b
FRC 4818 The Herd <wfrobotics@gmail.com>
2017-02-06 18:39:09 -0600

    Added a bunch of pseudocode for the logic of the vision shooting Added a constant that will be the correct tape seperation

--- src/org/wfrobotics/commands/VisionShoot.java
+++ src/org/wfrobotics/commands/VisionShoot.java
@@ -15,13 +15,27 @@ public class VisionShoot extends CommandGroup {
         
         if (data.InView)
         {
-            double yawOffset = data.Yaw;
-            // rotate the robot command thing here
-            addParallel(new AutoDrive(yawOffset, .05 * yawOffset));
             double pitchOffset = data.Pitch;
+            // sample correct tape separation
+            //get current tape separation
+            // if current separation is less than correct separation
+                //move closer (how much closer)
+            //else if current separation is greater than correct separation
+                //move farther (how much farther)
+            //else
+                //don't move
+            //we need to calculate the time based on the data.pitchOffset
+           
+            // rotate the robot command thing here
+            double yawOffset = data.Yaw;
+            addSequential(new AutoDrive(yawOffset, .05 * yawOffset));
+            //get correct tape point
+            //get current tape point
+            //using inverse sin find the angle based on the distance and offset of current and correct tape points
         }
         
     //check coordinates (tolerances)
+        
         //check distance (tolerances) 
         //if not pointing at boiler
             //turn towards the boiler
