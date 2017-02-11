76863ecfc2ebd877503902de691408b00c29b91e
FRC 4818 The Herd <wfrobotics@gmail.com>
2017-02-11 15:13:40 -0600

    Made the Camera subsystem which is a parent for Targeting and Aligning Made an outline for the AutoShoot command

--- src/org/wfrobotics/subsystems/Shooter.java
+++ src/org/wfrobotics/subsystems/Shooter.java
@@ -4,8 +4,6 @@ import org.wfrobotics.commands.Rev;
 import org.wfrobotics.robot.RobotMap;
 
 import com.ctre.CANTalon;
-import com.ctre.CANTalon.FeedbackDevice;
-import com.ctre.CANTalon.TalonControlMode;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
