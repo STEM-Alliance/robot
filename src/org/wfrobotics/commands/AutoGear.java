318b4656ca67d7640177c544345a4a61c678df00
David Lindner <David Lindner@localhost>
2017-02-11 18:15:11 -0600

    Extracted AutoGear robot position differences

--- src/org/wfrobotics/commands/AutoGear.java
+++ src/org/wfrobotics/commands/AutoGear.java
@@ -1,129 +1,80 @@
 package org.wfrobotics.commands;
 
-import org.wfrobotics.robot.Robot;
-
-
 import edu.wpi.first.wpilibj.command.CommandGroup;
-
-// TODO Add this command to the list of commands we can select from while we are setting up the robot per match
 
 /**
  * Autonomously score the gear
  * This command group attempts to position for and score a gear during the first 15 seconds of the game.
- * @author drlindne
- *
  */
 public class AutoGear extends CommandGroup
 {
-    enum STARTPOS {LEFT, CENTER, RIGHT};
-    
-    final STARTPOS startPos;
-    boolean isMoving = false;
-    
-    // TODO be smart enough to vary based on which starting position we are at
-        // Which means we will have some extra turning and different distances to move
-    
-    public AutoGear(STARTPOS startpos)
-    { 
-        startPos = startpos;
-        requires(Robot.driveSubsystem);
-        requires(Robot.aligningSubsystem);
-        //turn towards lift using angle constant
+    public static enum POSITION {LEFT, CENTER, RIGHT}
+    public class Config
+    {
+        public final double timeForward, timeToSpring, angleSpring;
         
-        if(startPos == STARTPOS.CENTER)
+        public Config(double timeForward, double timeToSpring, double angleSpring)
         {
-        addSequential(new AutoDrive(2, 1, 0, 1));
-            //Robot.driveSubsystem.driveXY(0, 1, 0);
-            isMoving = true;
-        //Time will most likely vary
+            this.timeForward = timeForward;
+            this.timeToSpring = timeToSpring;
+            this.angleSpring = angleSpring;
         }
-        else if (startPos == STARTPOS.LEFT)
-        {
-         
-            /* Vector vector = new Vector(0, 1);
-           Robot.driveSubsystem.driveWithHeading(vector, .5, 45);
-           */
-           addSequential(new AutoDrive(1, 1, 0, 2));
-           addSequential(new AutoDrive(Constants.AUTONOMOUS_TURN_SPEED, 45, Constants.AUTONOMOUS_TURN_TOLERANCE));
-           isMoving = true;
-            //TODO Fill in
-        }
-        else if (startPos == STARTPOS.RIGHT)
-        {
-            
-            /*
-            Vector vector = new Vector(0, 1);
-            Robot.driveSubsystem.driveWithHeading(vector, -.5, 315);
-            */
-            addSequential(new AutoDrive(1, 1, 0, 2));
-            addSequential(new AutoDrive(Constants.AUTONOMOUS_TURN_SPEED, 315, Constants.AUTONOMOUS_TURN_TOLERANCE));
-            isMoving = true;
-        }
-        //take picture and retrieve yaw
+    }    
+
+    public AutoGear(POSITION startingPosition)
+    {
+        Config config = selectConfig(startingPosition);
         
-        //shift left/right
-        //drive forwards
-        //stop for 4 seconds
-        // TODO CHEW ON THIS STUDENTS! 
-            // This is a great command for you to perfect because you can easily (just deploy changes)
-            // and safely test this. Watch out for pesky school walls and other students, they are negative points. 
+        // Drive forwards while turning towards the airship's spring
+        addSequential(new AutoDrive(0, Constants.AUTONOMOUS_DRIVE_SPEED, Constants.AUTONOMOUS_TURN_SPEED, config.angleSpring, config.timeForward));
         
-        // What do?
-        // TODO Conditionally turn?
-        // TODO Move towards airship
-        // TODO Conditionally turn?
+        // Drive towards the spring directly ahead
+        if(config.timeToSpring > 0)
+        {
+            addSequential(new AutoDrive(0, Constants.AUTONOMOUS_DRIVE_SPEED, Constants.AUTONOMOUS_TURN_SPEED, config.angleSpring, config.timeForward));
+        }
+        
+        // We are at the gear. Score it
+        addSequential(new VisionGearDropOff());  // TODO should we put this in a special mode or cancel this if we near the end of autonomous without scoring?
+
+        // If there is any time left, we should shoot or start moving to our next destination (like getting another gear)
+        // TODO
+        
+        
+        
         // TODO While we have not scored? Maybe we only get one chance?
-            // Perhaps the body of this is a function that adds commands or it's own private Class CommandGroup
-            // TODO Move towards spring
-                // TODO Ultrasonic black magic to do this? Not only sense distance but that we are square?
-                // TODO Or Camera? There's those reflective tape strips centered on the spring.
-            // TODO Detect spring?
-                // TODO If we make it, double check this command will alert the human player with LEDs
-                // TODO Then we are going to want to leave this loop
-            // TODO reset???
+        // Perhaps the body of this is a function that adds commands or it's own private Class CommandGroup
+        // TODO Move towards spring
+        // TODO Ultrasonic black magic to do this? Not only sense distance but that we are square?
+        // TODO Or Camera? There's those reflective tape strips centered on the spring.
+        // TODO Detect spring?
+        // TODO If we make it, double check this command will alert the human player with LEDs
+        // TODO Then we are going to want to leave this loop
+        // TODO reset???
         // TODO Back out
-            // TODO If we have scored, this condones serious flaunting (of our LEDs)?
+        // TODO If we have scored, this condones serious flaunting (of our LEDs)?
         // TODO Turn towards the boiler?
-            // TODO if we are going to shoot, revving while turning would save time
+        // TODO if we are going to shoot, revving while turning would save time
         // TODO Should we try to shoot? Should this be optional? Should this be smart?
     }
     
-    protected void execute()
+    private Config selectConfig(POSITION startingPosition)
     {
-        if(isMoving == true)
+        if (startingPosition == POSITION.CENTER)
         {
-            if(Robot.aligningSubsystem.getData().Yaw > 0) // too far to the right
-            {
-                Robot.driveSubsystem.driveXY(-0.2, 0, 0);                
-            }
-            else if(Robot.aligningSubsystem.getData().Yaw < 0)// too far to the left
-            {
-                Robot.driveSubsystem.driveXY(0.2, 0, 0);
-            }
-            else if(Robot.aligningSubsystem.getData().Yaw == 0)
-            {
-                if(Robot.targetingSubsystem.DistanceToTarget() > 0)
-                {
-                    Robot.driveSubsystem.driveXY(0, 0.5, 0);
-                }
-                else if(Robot.targetingSubsystem.DistanceToTarget() == 0)
-                {
-                    Robot.driveSubsystem.driveXY(0, 0, 0);
-                    isMoving = false;
-                }
-            }
+            return new Config(3, 0, 0);
+        }
+        else if(startingPosition == POSITION.LEFT)
+        {
+            return new Config(5, 2, 45);
+        }
+        else if(startingPosition == POSITION.RIGHT)
+        {
+            return new Config(5, 2, -45);
+        }
+        else
+        {
+            return new Config(0, 0, 0);
         }
     }
-    
-    protected void end()
-    {
-        Robot.driveSubsystem.driveXY(0, 0, 0);
-    }
-    
-    protected void interrupted()
-    {
-        // TODO?
-    }
-
 }
-
--- src/org/wfrobotics/commands/VisionGearDropOff.java
+++ src/org/wfrobotics/commands/VisionGearDropOff.java
@@ -5,51 +5,72 @@ import org.wfrobotics.subsystems.Camera.TargetData;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-/**
- *
- */
-public class VisionGearDropOff extends CommandGroup {
-
-    public VisionGearDropOff() {
-        TargetData data = Robot.targetingSubsystem.getData();
-        
-    if(data.InView)
+public class VisionGearDropOff extends CommandGroup 
+{
+    public VisionGearDropOff() 
     {
-        double yawOffset = data.Yaw; //rotate control
-        double tolerance = Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE * .05;
+        TargetData data = Robot.targetingSubsystem.getData();
 
-        addSequential(new AutoDrive(Constants.AUTONOMOUS_TURN_SPEED, yawOffset, Constants.AUTONOMOUS_TURN_TOLERANCE));
-
-        //distance to boiler 
-
-        if(Math.abs(Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE - Robot.targetingSubsystem.DistanceToTarget()) <= tolerance )
+        if(data.InView)
         {
+            double yawOffset = data.Yaw; //rotate control
+            double tolerance = Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE * .05;
 
-            if(Robot.targetingSubsystem.DistanceToTarget() < Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE )                
+            addSequential(new AutoDrive(Constants.AUTONOMOUS_TURN_SPEED, yawOffset, Constants.AUTONOMOUS_TURN_TOLERANCE));
+
+            //distance to boiler 
+
+            if(Math.abs(Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE - Robot.targetingSubsystem.DistanceToTarget()) <= tolerance )
             {
-                Robot.driveSubsystem.driveXY(0, .3, -1);            
-            }
-            else if(Robot.targetingSubsystem.DistanceToTarget() > Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE )
-            {
-                Robot.driveSubsystem.driveXY(0, -.3, -1);            
-            }
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
 
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
+                if(Robot.targetingSubsystem.DistanceToTarget() < Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE )                
+                {
+                    Robot.driveSubsystem.driveXY(0, .3, -1);            
+                }
+                else if(Robot.targetingSubsystem.DistanceToTarget() > Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE )
+                {
+                    Robot.driveSubsystem.driveXY(0, -.3, -1);            
+                }
+                // Add Commands here:
+                // e.g. addSequential(new Command1());
+                //      addSequential(new Command2());
+                // these will run in order.
 
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+                // To run multiple commands at the same time,
+                // use addParallel()
+                // e.g. addParallel(new Command1());
+                //      addSequential(new Command2());
+                // Command1 and Command2 will run in parallel.
+
+                // A command group will require all of the subsystems that each member
+                // would require.
+                // e.g. if Command1 requires chassis, and Command2 requires arm,
+                // a CommandGroup containing them would require both the chassis and the
+                // arm.
+            }
         }
     }
+    
+    protected void execute()
+    {
+//        if(Robot.aligningSubsystem.getData().Yaw > 0) // too far to the right
+//        {
+//            Robot.driveSubsystem.driveXY(-0.2, 0, 0);                
+//        }
+//        else if(Robot.aligningSubsystem.getData().Yaw < 0)// too far to the left
+//        {
+//            Robot.driveSubsystem.driveXY(0.2, 0, 0);
+//        }
+//        else if(Robot.aligningSubsystem.getData().Yaw == 0)
+//        {
+//            if(Robot.targetingSubsystem.DistanceToTarget() > 0)
+//            {
+//                Robot.driveSubsystem.driveXY(0, 0.5, 0);
+//            }
+//            else if(Robot.targetingSubsystem.DistanceToTarget() == 0)
+//            {
+//                Robot.driveSubsystem.driveXY(0, 0, 0);
+//            }
+//        }
     }
 }
