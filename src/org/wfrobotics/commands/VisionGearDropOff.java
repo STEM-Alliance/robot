cb86a122f4da9ad37eda094db8fc8325e9865664
FRC 4818 The Herd <wfrobotics@gmail.com>
2017-02-25 17:15:03 -0600

    Vision Gear workingish but still broken.

--- src/org/wfrobotics/commands/VisionGearDropOff.java
+++ src/org/wfrobotics/commands/VisionGearDropOff.java
@@ -1,17 +1,21 @@
 package org.wfrobotics.commands;
 
+import org.wfrobotics.PIDController;
 import org.wfrobotics.commands.drive.AutoDrive;
 import org.wfrobotics.subsystems.Camera.TargetData;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class VisionGearDropOff extends CommandGroup 
 {    
     private GearDetection camera;
     private AutoDrive drive;
+    private PIDController pid;
     
     public VisionGearDropOff() 
     {
+        pid = new PIDController(1.6, 0.0015, 0.0001, .5);
         camera = new GearDetection(GearDetection.MODE.GETDATA);
         drive = new AutoDrive(0, 0, 0, 0, 999);
         
@@ -21,8 +25,20 @@ public class VisionGearDropOff extends CommandGroup
     
     protected void execute()
     {
-        //TargetData data = camera.getData();  // TODO grab the data from the command
+        double distanceFromCenter = camera.getDistanceFromCenter();
+        double valueY = 0;
+        double valueX = 0;
         
-        drive.set(0, 0, 0, 0);  // TODO update AutoDrive after we add that ability to autodrive
+        if(camera.getIsFound())
+        {
+            valueY = pid.update(distanceFromCenter);
+        }
+        
+        if(Math.abs(distanceFromCenter)<.15)
+        {
+            valueX = -.4;
+        }
+        
+        drive.set(valueX, valueY, 0, -1);  // TODO update AutoDrive after we add that ability to autodrive
     }
 }
