0c4df9a40b5a78a787359c1300d95fb4865e8e7a
wfrobotics <wfrobotics@gmail.com>
2017-02-25 14:07:16 -0600

    Vision updates.

--- src/org/wfrobotics/commands/VisionGearDropOff.java
+++ src/org/wfrobotics/commands/VisionGearDropOff.java
@@ -5,71 +5,76 @@ import org.wfrobotics.subsystems.Camera.TargetData;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class VisionGearDropOff extends CommandGroup 
-{
-    enum STATE {YAW, APPROACH, SPRING};
+public class VisionGearDropOff extends CommandGroup {
+    enum STATE {
+        YAW, APPROACH, SPRING
+    };
+
     STATE state;
-    //comment
-    TargetData data;
+    // comment
     double tolerance;
-    
-    public VisionGearDropOff() 
+
+    public VisionGearDropOff()
     {
-        data = Robot.targetingSubsystem.getData();
-        state = STATE.YAW;
-       
-       /* if(data.InView)
-        {
-            double yawOffset = data.Yaw; //rotate control
-            //double tolerance = Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE * 0.05;
+        Robot.targetGearSubsystem.run();
 
-            addSequential(new AutoDrive(Constants.AUTONOMOUS_TURN_SPEED, yawOffset, Constants.AUTONOMOUS_TURN_TOLERANCE));
-
-            //distance to boiler 
-
-            if(Math.abs(Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE - Robot.targetingSubsystem.DistanceToTarget()) <= tolerance )
-            {
-
-                if(Robot.targetingSubsystem.DistanceToTarget() < Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE )                
-                {
-                    Robot.driveSubsystem.driveXY(0, .3, -1);            
-                }
-                else if(Robot.targetingSubsystem.DistanceToTarget() > Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE )
-                {
-                    Robot.driveSubsystem.driveXY(0, -.3, -1);            
-                }
-            }
-        }*/
+        /*
+         * if(data.InView) { double yawOffset = data.Yaw; //rotate control
+         * //double tolerance = Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE * 0.05;
+         * 
+         * addSequential(new AutoDrive(Constants.AUTONOMOUS_TURN_SPEED,
+         * yawOffset, Constants.AUTONOMOUS_TURN_TOLERANCE));
+         * 
+         * //distance to boiler
+         * 
+         * if(Math.abs(Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE -
+         * Robot.targetingSubsystem.DistanceToTarget()) <= tolerance ) {
+         * 
+         * if(Robot.targetingSubsystem.DistanceToTarget() <
+         * Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE ) {
+         * Robot.driveSubsystem.driveXY(0, .3, -1); } else
+         * if(Robot.targetingSubsystem.DistanceToTarget() >
+         * Constants.OPTIMAL_GEAR_DROP_OFF_DISTANCE ) {
+         * Robot.driveSubsystem.driveXY(0, -.3, -1); } } }
+         */
     }
-    
+
     protected void execute()
     {
-        switch(state)
+        double xDistance = Robot.targetGearSubsystem.DistanceFromCenter;
+        switch (state)
         {
             case YAW:
-               if(data.Yaw > 0) //too far to the right
-               {
-                   addSequential( new AutoDrive(-.25, 0, 0, 0, 1));
-               }else if(data.Yaw < 0) // too far to the left
-               {
-                   addSequential( new AutoDrive(.25, 0, 0, 0, 1));
-               }else if(data.Yaw == 0)
-               {
-                   state = STATE.APPROACH;   
-               }
+                
+                if(Robot.targetGearSubsystem.InView)
+                {
+                    if (xDistance > 0.1) // too far to the right
+                    {
+                        addSequential(new AutoDrive(-.25, 0, 0, 0, 1));
+                    }
+                    else if (xDistance < 0.1) // too far to the left
+                    {
+                        addSequential(new AutoDrive(.25, 0, 0, 0, 1));
+                    }
+                    else
+                    {
+                        state = STATE.APPROACH;
+                    }
+                }
                 break;
             case APPROACH:
-                if(data.Depth == 0 | Robot.driveSubsystem.isSpringInGear())
-                {
-                    state = STATE.SPRING;
-                }
-               else if(data.Depth > 0)
-               {
-                   addSequential( new AutoDrive(0, .25, 0, 0, .5));
-               }
+                addSequential(new AutoDrive(0, .25, 0, 0, .5));
+                // if(data.Depth == 0 | Robot.driveSubsystem.isSpringInGear())
+                // {
+                // state = STATE.SPRING;
+                // }
+                // else if(data.Depth > 0)
+                // {
+                // addSequential( new AutoDrive(0, .25, 0, 0, .5));
+                // }
                 break;
             case SPRING:
-                if(!Robot.driveSubsystem.isGearStored())
+                if (!Robot.driveSubsystem.isGearStored())
                 {
                     isFinished();
                 }
---
+++ src/org/wfrobotics/vision/TargetTable.java
@@ -0,0 +1,41 @@
+package org.wfrobotics.vision;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class TargetTable {
+
+    NetworkTable table;
+    
+    public String name;
+
+    //TODO figure out how to get this automatically
+    public double imageWidth = 640;
+    public double imageHeight = 480;
+    
+    public double[] x = {0};
+    public double[] y = {0};
+    public double[] height = {0};
+    public double[] width = {0};
+    public double[] solidity = {0};
+    public double[] area = {0};
+    
+    public int targetsFound = 0;
+    
+    public TargetTable(String name)
+    {
+        table = NetworkTable.getTable("GRIP/" + name);
+    }
+    
+    public void update()
+    {
+        x = table.getNumberArray("centerX", x);
+        y = table.getNumberArray("centerY", y);
+        height  = table.getNumberArray("height", height);
+        width = table.getNumberArray("width", width);
+        solidity = table.getNumberArray("solidity", solidity);
+        area = table.getNumberArray("area", area);
+        
+        targetsFound = Math.min(x.length, Math.min(y.length, Math.min(height.length, width.length)));
+    }
+
+}
