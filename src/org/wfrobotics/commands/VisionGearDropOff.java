74bebcede56a0fff0675666ace467e65baa36b45
David Lindner <David Lindner@localhost>
2017-02-27 17:16:52 -0600

    Dead reckoning auto gear

--- src/org/wfrobotics/commands/VisionGearDropOff.java
+++ src/org/wfrobotics/commands/VisionGearDropOff.java
@@ -3,17 +3,19 @@ package org.wfrobotics.commands;
 import org.wfrobotics.PIDController;
 import org.wfrobotics.commands.drive.AutoDrive;
 import org.wfrobotics.robot.Robot;
-import org.wfrobotics.subsystems.Camera.TargetData;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class VisionGearDropOff extends CommandGroup 
 {     
+    final static double HEXAGON_ANGLE = 30;  // All corners are 120 on the interior, therefore the sides we want are 30 degrees past straight ahead
+    private enum STATE {APPROACH, WAIT_FOR_HUMAN, BACKOUT};
+    
     private GearDetection camera;
     private AutoDrive drive;
     private PIDController pid;
     private double heading;
+    private STATE state;
     
     public VisionGearDropOff() 
     {
@@ -29,37 +31,54 @@ public class VisionGearDropOff extends CommandGroup
     {
         double robotHeading = Robot.driveSubsystem.getLastHeading();
         
-        if (robotHeading < 67.5 && robotHeading > 22.5)  
+        // Angle to snap to, surrounding each spring, generous on the sides. 
+        // Ex: Spring at 30 degrees, technically range could be 15-45 degrees, allow 15-135 since this is the outside spring
+        if (robotHeading < HEXAGON_ANGLE * 3 && robotHeading > HEXAGON_ANGLE * .5)
         {
-            heading = 45;  // Snap to left spring
+            heading = HEXAGON_ANGLE;  // Snap to left spring
         }
-        else if (robotHeading > -67.5 && robotHeading < -22.5)  
+        else if (robotHeading > -HEXAGON_ANGLE * 3 && robotHeading < -HEXAGON_ANGLE * .5)  
         {
-            heading = -45;  // Snap to right spring
+            heading = -HEXAGON_ANGLE;  // Snap to right spring
         }
         else
         {
             heading = 0;
         }
+        state = STATE.APPROACH;
     }
     
     protected void execute()
     {
-        double distanceFromCenter = camera.getDistanceFromCenter();
-        double valueY = 0;
         double valueX = 0;
+        double valueY = 0;
         
-        if(camera.getIsFound())
+        if (state == STATE.APPROACH)
         {
-            valueY = pid.update(distanceFromCenter);
+            double distanceFromCenter = camera.getDistanceFromCenter();
+            
+            if(camera.getIsFound())
+            {
+                valueY = pid.update(distanceFromCenter);
+            }
+            
+            if(Math.abs(distanceFromCenter) < .15)
+            {
+                valueX = -.4;
+            }
+            
+            // TODO apply rotational speed and heading to become parallel to spring
+            // TODO If we get a SPRING_IN_GEAR sensor, convert this into a approach-wait-retry state machine
+        }
+        else if (state == STATE.WAIT_FOR_HUMAN)
+        {
+            // Not supported yet; we just do STATE.APPROACH
+        }
+        else if (state == STATE.BACKOUT)
+        {
+            // Not supported yet; we just do STATE.APPROACH
         }
         
-        if(Math.abs(distanceFromCenter) < .15)
-        {
-            valueX = -.4;
-        }
-        
-        //drive.set(valueX, valueY, getRotationalSpeed, heading);  // TODO update AutoDrive after we add that ability to autodrive
         drive.set(valueX, valueY, 0, -1);  // TODO update AutoDrive after we add that ability to autodrive
     }
     
