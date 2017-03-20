144e97dc74dff2c3ba00bd46937bd7bce1a7f524
David Lindner <David Lindner@localhost>
2017-03-20 18:04:31 -0500

    Cleanup vision shoot

--- src/org/wfrobotics/commands/VisionShoot.java
+++ src/org/wfrobotics/commands/VisionShoot.java
@@ -9,7 +9,6 @@ import org.wfrobotics.hardware.led.LEDs.Effect;
 import org.wfrobotics.hardware.led.LEDs.Effect.EFFECT_TYPE;
 import org.wfrobotics.robot.Robot;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -35,81 +34,24 @@ public class VisionShoot extends CommandGroup
     
     protected void initialize()
     {
-        done = false;
-        
-        startTime = Timer.getFPGATimestamp();
+        done = false;        
+        startTime = timeSinceInitialized();
+        atAngleTime = timeSinceInitialized();
         Robot.leds.set(new Effect(EFFECT_TYPE.OFF, LEDs.BLACK, 1));
         pidRotate.resetError();
     }
     
     protected void execute()
     {
-        
         double distanceFromCenter = camera.getDistanceFromCenter();
-        double visionWidth = camera.getFullWidth();
-        double valueAngle = 0;
 
-        if(Timer.getFPGATimestamp() - startTime > 1 && camera.getIsEnabled())
+        if (!isCameraReady())
         {
-            if(camera.getIsFound())
-            {
-                // we think we've found at least one target
-                SmartDashboard.putNumber("ShooterCenter", distanceFromCenter);
-    
-                // so get an estimate speed to line us up
-                valueAngle = -pidRotate.update(distanceFromCenter);
-    
-                if(Math.abs(distanceFromCenter) < .1)
-                {
-                    // if we started really far away from center, 
-                    // this will then reduce that overshoot
-                    // maybe
-                    //pidRotate.resetError();
-                }
-    
-                // try and see if we've been at the target for long enough
-                if(Math.abs(distanceFromCenter) < .1)
-                {
-                    if(Timer.getFPGATimestamp() - atAngleTime > .5)
-                    {
-                        done = true;
-                    }
-                }
-                else
-                {
-                    atAngleTime = Timer.getFPGATimestamp();
-                }
-                
-                // we can still see a target
-                if(visionWidth > 15)
-                {
-                    SmartDashboard.putNumber("ShooterAngle", valueAngle);
-                    //TODO this only works for the front facing spring
-                    // address field heading here
-                    rotate.set(new Vector(0, 0), valueAngle, -1);
-                }
-                else
-                {
-                    // if the detected target width is less than 15, 
-                    // we're either at the target
-                    // or something went wrong
-                    done = true;
-                    rotate.set(new Vector(0, 0), 0, -1);
-                }
-            }
-            else
-            {
-                done = true;
-            }
-        }
-        else if (Timer.getFPGATimestamp() - startTime > 4)
-        {
-            // timeout after 4 seconds of waiting for the camera to switch
-            done = true;
+            rotate.set(new Vector(0, 0), 0, -1);
         }
         else
         {
-            rotate.set(new Vector(0, 0), 0, -1);
+            doVisionDrive();
         }
         
         Utilities.PrintCommand("VisionShoot", this, distanceFromCenter + " " + done);
@@ -129,6 +71,74 @@ public class VisionShoot extends CommandGroup
     {
         end();
     }
+    
+    public boolean isCameraReady()
+    {
+        double now = timeSinceInitialized();
+        boolean found = true;
 
+        if (now - startTime < 1 || !camera.getIsEnabled())
+        {
+            found = false;
+            
+            if (now - startTime > 4)
+            {
+                done = true;
+            }
+        }
+        else if (!camera.getIsFound())
+        {
+            found = false;
+            done = true;
+        }
+        
+        return found;
+    }
+    
+    public void doVisionDrive()
+    {
+        double distanceFromCenter = camera.getDistanceFromCenter();
+        double visionWidth = camera.getFullWidth();
+        double valueAngle = 0;
+        double setPointAngle;
+        
+        // we think we've found at least one target
+        SmartDashboard.putNumber("ShooterCenter", distanceFromCenter);
+
+        // so get an estimate speed to line us up
+        valueAngle = -pidRotate.update(distanceFromCenter);
+
+        if(Math.abs(distanceFromCenter) < .1)
+        {
+            // if we started really far away from center, this will then reduce that overshoot maybe
+            //pidRotate.resetError();
+        }
+
+        // try and see if we've been at the target for long enough
+        if(Math.abs(distanceFromCenter) > .1)
+        {
+            atAngleTime = timeSinceInitialized();
+        }
+        else if(timeSinceInitialized() - atAngleTime > .5)
+        {
+            done = true;
+        }
+        
+        // we can still see a target
+        if(visionWidth > 15)
+        {
+            SmartDashboard.putNumber("ShooterAngle", valueAngle);
+            setPointAngle = valueAngle;
+            //TODO this only works for the front facing spring address field heading here            
+        }
+        else
+        {
+            // if the detected target width is less than 15, we're either at the target or something went wrong
+            done = true;
+            setPointAngle = 0;
+        }
+        
+        rotate.set(new Vector(0, 0), setPointAngle, -1);
+    }
 }
 
