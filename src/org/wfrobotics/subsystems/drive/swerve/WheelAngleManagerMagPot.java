86cbe6ea787a65b383e5734565a9c77b7c84b709
wfrobotics <wfrobotics@gmail.com>
2017-03-23 14:29:01 -0500

    Swerve to mag pots, cal shifters

---
+++ src/org/wfrobotics/subsystems/drive/swerve/WheelAngleManagerMagPot.java
@@ -0,0 +1,66 @@
+package org.wfrobotics.subsystems.drive.swerve;
+
+import org.wfrobotics.Utilities;
+import org.wfrobotics.hardware.MagnetoPot;
+import org.wfrobotics.hardware.MagnetoPotSRX;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.util.AllocationException;
+
+public class WheelAngleManagerMagPot 
+{
+    private CANTalon angleMotor;
+    private MagnetoPot anglePot;
+    /** Invert the angle motor and sensor to swap left/right */
+    private boolean angleInverted = true;
+    
+    public WheelAngleManagerMagPot(int talonAddress)
+    {
+        angleMotor = new CANTalon(talonAddress);
+        //angleMotor.setVoltageRampRate(30);
+        angleMotor.configNominalOutputVoltage(0, 0);
+        angleMotor.configPeakOutputVoltage(11, -11);
+        angleMotor.ConfigFwdLimitSwitchNormallyOpen(true);
+        angleMotor.ConfigRevLimitSwitchNormallyOpen(true);
+        angleMotor.enableForwardSoftLimit(false);
+        angleMotor.enableReverseSoftLimit(false);
+        angleMotor.enableBrakeMode(false);
+        //angleMotor.configNominalOutputVoltage(SwerveWheel.MINIMUM_SPEED, -SwerveWheel.MINIMUM_SPEED);  // Hardware deadband in closed-loop modes
+        //angleMotor.SetVelocityMeasurementPeriod(CANTalon.VelocityMeasurementPeriod.Period_50Ms);
+        //angleMotor.SetVelocityMeasurementWindow(32);
+        
+        anglePot = new MagnetoPotSRX(angleMotor, 360);
+    }
+    
+    public void set(double speed)
+    {
+        double invert = angleInverted ? -1 : 1;
+        angleMotor.set(invert * speed);
+    }
+
+    public double getAnglePotAdjusted()
+    {
+        double invert = angleInverted ? 1 : -1;
+        
+        return Utilities.round(Utilities.wrapToRange(invert * anglePot.get(),-180,180),2);
+    }
+    
+    public void setPotOffset(double offset)
+    {
+        anglePot.setOffset(offset);
+    }
+    
+    public double debugGetPotRaw()
+    {
+        return anglePot.getRawInput();
+    }
+    
+    /**
+     * For unit tests until we mock the pot
+     */
+    public void free()
+    {
+        try { anglePot.free(); } catch (AllocationException e) {}
+    }
+}
