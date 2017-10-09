e7a41829fc7b813e34e69bfbc3c6fb446a84f9f0
David Lindner <lindnerdavidr@johndeere.com>
2017-10-09 01:15:12 -0500

    Prevent unintended pass by ref

--- src/org/wfrobotics/reuse/subsystems/swerve/SwerveSignal.java
+++ src/org/wfrobotics/reuse/subsystems/swerve/SwerveSignal.java
@@ -5,7 +5,7 @@ import org.wfrobotics.reuse.utilities.HerdVector;
 public class SwerveSignal
 {
     public final static double HEADING_IGNORE = -1;
-    
+
     HerdVector velocity;    // Direction
     double spin;            // Twist while moving
     double heading;         // Angle to track
@@ -13,7 +13,7 @@ public class SwerveSignal
     public SwerveSignal(HerdVector velocity,  double rotationalSpeed, double heading)
     {
         this.velocity = new HerdVector(velocity);
-        this.spin = rotationalSpeed;
+        spin = rotationalSpeed;
         this.heading = heading;
     }
 
@@ -21,19 +21,21 @@ public class SwerveSignal
     {
         this(velocity, rotationalSpeed, HEADING_IGNORE);
     }
-    
+
+    public SwerveSignal(SwerveSignal clone)
+    {
+        this(clone.velocity, clone.spin, clone.heading);
+    }
+
     public String toString()
     {
         if(hasHeading())
         {
-            return String.format("V: %s, W: %.2f, H: %.2f", velocity, spin, heading);    
+            return String.format("V: %s, W: %.2f, H: %.2f", velocity, spin, heading);
         }
-        else
-        {
-            return String.format("V: %s, W: %.2f, H: None", velocity, spin);
-        }
+        return String.format("V: %s, W: %.2f, H: None", velocity, spin);
     }
-    
+
     public boolean hasHeading()
     {
         return heading != HEADING_IGNORE;
