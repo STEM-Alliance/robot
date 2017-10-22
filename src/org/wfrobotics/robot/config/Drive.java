3df4a27665fe3b337eb08baa5321b141fcf038f3
David Lindner <lindnerdavidr@johndeere.com>
2017-10-21 20:38:22 -0500

    Robot specific drive config

---
+++ src/org/wfrobotics/robot/config/Drive.java
@@ -0,0 +1,8 @@
+package org.wfrobotics.robot.config;
+
+public class Drive
+{
+    public static boolean AUTO_SHIFT_ENABLE = false;
+    public static double AUTO_SHIFT_TIME = 1;
+    public static double AUTO_SHIFT_SPEED = .5;
+}
