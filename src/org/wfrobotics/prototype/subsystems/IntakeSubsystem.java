a07218e836f7d11c773b6be4e2de7a7499d3e8e2
FRC 4818 The Herd <wfrobotics@gmail.com>
2018-01-15 13:27:14 -0600

    Optimized Intake code

--- src/org/wfrobotics/prototype/subsystems/IntakeSubsystem.java
+++ src/org/wfrobotics/prototype/subsystems/IntakeSubsystem.java
@@ -1,46 +1,44 @@
 package org.wfrobotics.prototype.subsystems;
 
-import org.wfrobotics.prototype.commands.BoxIn;
-import org.wfrobotics.prototype.commands.BoxOut;
-import org.wfrobotics.prototype.commands.ExampleForwardCommand;
 import org.wfrobotics.prototype.commands.Wait;
-import org.wfrobotics.reuse.controller.ButtonFactory;
-import org.wfrobotics.reuse.controller.Xbox;
-import org.wfrobotics.reuse.controller.ButtonFactory.TRIGGER;
 
 import com.ctre.CANTalon;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
-import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class IntakeSubsystem extends Subsystem {
-
+    CANTalon RightIntake;
+    CANTalon LeftIntake;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-     CANTalon RightIntake = new CANTalon(1);
-     CANTalon LeftIntake = new CANTalon(2);
-
+    public IntakeSubsystem()
+    {
+      RightIntake = new CANTalon(1);
+      LeftIntake = new CANTalon(2);
+      RightIntake.setInverted(true);
+      LeftIntake.set(ControlMode.Follower, 1);
+        // TODO Auto-generated constructor stub
+    }
+    
+     public boolean hasCube(){
+         return false;
+     }
     public void initDefaultCommand() {
         setDefaultCommand(new Wait());
         
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
-    public void rightSetSpeed(double rpm)
+    public void SetSpeed(double rpm)
     {
        RightIntake.set(ControlMode.PercentOutput, rpm);
+      
     }
-    public void leftSetSpeed(double rpm)
-    {
-       LeftIntake.set(ControlMode.PercentOutput, rpm);
-    }
-    public static Xbox controller = new Xbox(0);
-
-    public static Button X = ButtonFactory.makeButton(controller, Xbox.BUTTON.X, TRIGGER.WHILE_HELD, new BoxIn());
-    public static Button Y = ButtonFactory.makeButton(controller, Xbox.BUTTON.Y, TRIGGER.WHILE_HELD, new BoxOut());
+  
+    
 }
 
