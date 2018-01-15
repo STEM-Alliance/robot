d0c337c62e8160d55651a360907d9dca88cec279
Brad Poppe <poppeseed@gmail.com>
2018-01-14 22:28:14 -0600

    Updated for 2018.

--- src/org/wfrobotics/robot/subsystems/MecanumSubsystem.java
+++ src/org/wfrobotics/robot/subsystems/MecanumSubsystem.java
@@ -1,12 +1,13 @@
 package org.wfrobotics.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 import org.wfrobotics.reuse.hardware.sensors.Gyro;
 import org.wfrobotics.reuse.utilities.HerdVector;
 import org.wfrobotics.robot.commands.DriveMecanum;
 
-import com.ctre.CANTalon;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.*;
 
@@ -14,30 +15,30 @@ import edu.wpi.first.wpilibj.*;
  *
  */
 public class MecanumSubsystem extends Subsystem {
-    RobotDrive drive;
-    CANTalon frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor;
+    MecanumDrive drive;
+    WPI_TalonSRX frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor;
     public Gyro gyro;
     
     public MecanumSubsystem()
     {
-        this.frontLeftMotor = new CANTalon(10);
-        this.rearLeftMotor = new CANTalon(11);
-        this.frontRightMotor = new CANTalon(12);
-        this.rearRightMotor = new CANTalon(13);
+        this.frontLeftMotor = new WPI_TalonSRX(10);
+        this.rearLeftMotor = new WPI_TalonSRX(11);
+        this.frontRightMotor = new WPI_TalonSRX(12);
+        this.rearRightMotor = new WPI_TalonSRX(13);
         this.gyro = Gyro.getInstance();
 
         this.rearLeftMotor.setInverted(true);
         this.frontLeftMotor.setInverted(true);
-        drive = new RobotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
-        double rampRate = 40;
-        frontRightMotor.setVoltageRampRate(rampRate);
-        frontLeftMotor.setVoltageRampRate(rampRate);
-        rearRightMotor.setVoltageRampRate(rampRate);
-        rearLeftMotor.setVoltageRampRate(rampRate);
+        drive = new MecanumDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+        double rampRate = .5; //TODO is this right? seconds from neutral to full
+        frontRightMotor.configOpenloopRamp(rampRate, 0);
+        frontLeftMotor.configOpenloopRamp(rampRate, 0);
+        rearRightMotor.configOpenloopRamp(rampRate, 0);
+        rearLeftMotor.configOpenloopRamp(rampRate, 0);
     }
     public void driveWithHeading(HerdVector vector, double rotation)
     {
-        drive.mecanumDrive_Polar(
+        drive.drivePolar(
                 vector.getMag(),
                 vector.getAngle() - this.gyro.getYaw(),
                 rotation);
@@ -45,7 +46,7 @@ public class MecanumSubsystem extends Subsystem {
 
     public void drive(HerdVector vector, double rotation)
     {
-        drive.mecanumDrive_Polar(
+        drive.drivePolar(
                 vector.getMag(),
                 vector.getAngle(),
                 rotation);
