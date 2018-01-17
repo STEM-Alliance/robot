4abeb86d4e8e857e11728480dc2146095a306841
David Lindner <lindnerdavidr@johndeere.com>
2018-01-17 08:48:12 -0600

    Add distance driven robot state

--- src/org/wfrobotics/reuse/subsystems/vision/messages/VisionMessageTargets.java
+++ src/org/wfrobotics/reuse/subsystems/vision/messages/VisionMessageTargets.java
@@ -5,75 +5,59 @@ import java.util.List;
 
 public class VisionMessageTargets
 {
-    public int Version;
-    public int Source;
-    public int ImageWidth;
-    public int ImageHeight;
-    public double FPS;
-    public double Timestamp;
+    public int version;
+    public int source;
+    public int imageWidth;
+    public int imageHeight;
+    public double fps;
+    public double timestamp;
     public List<VisionTargetInfo> Targets = new ArrayList<VisionTargetInfo>();
-    
-    public final String msg;
-    
 
-    /**
-     * Empty constructor
-     */
+    public final String msg;
+
     public VisionMessageTargets()
     {
-        this.msg = "";
-        this.Version = 0;
-        this.Source = 0;
-        this.ImageWidth = 0;
-        this.ImageHeight = 0;
-        this.FPS = 0;
-        this.Timestamp = 0;
-        this.Targets = new ArrayList<VisionTargetInfo>();
+        msg = "";
+        version = 0;
+        source = 0;
+        imageWidth = 0;
+        imageHeight = 0;
+        fps = 0;
+        timestamp = 0;
+        Targets = new ArrayList<VisionTargetInfo>();
     }
-    
+
     /**
-     * Parse a Targets message
+     * Parse a Target's message
      * @param msg raw message string
      */
     public VisionMessageTargets(String msg)
     {
         this.msg = msg;
-        this.Targets = new ArrayList<VisionTargetInfo>();
-        
+        Targets = new ArrayList<VisionTargetInfo>();
+
         // Message is formatted as comma separated list of
         // MsgLength,Version,Source,ImageWidth,ImageHeight,FPS,Timestamp,TargetCount,{Targets}
         // Targets repeat and are formatted as
         // Index,CenterX,CenterY,Width,Height
         String[] parts = msg.split(",");
         int index = 0;
-        
-        @SuppressWarnings("unused")
-        int messageLen = Integer.parseInt(parts[index++]);
-        
-        this.Version = Integer.parseInt(parts[index++]);
-        
-        this.Source = Integer.parseInt(parts[index++]);
 
-        this.ImageWidth = Integer.parseInt(parts[index++]);
-        
-        this.ImageHeight = Integer.parseInt(parts[index++]);
+        Integer.parseInt(parts[index++]);
+        version = Integer.parseInt(parts[index++]);
+        source = Integer.parseInt(parts[index++]);
+        imageWidth = Integer.parseInt(parts[index++]);
+        imageHeight = Integer.parseInt(parts[index++]);
+        fps = Double.parseDouble(parts[index++]);
+        timestamp = Double.parseDouble(parts[index++]);
 
-        this.FPS = Double.parseDouble(parts[index++]);
-        
-        this.Timestamp = Double.parseDouble(parts[index++]);
-        
         int count = Integer.parseInt(parts[index++]);
-        
+
         for(int i = 0; i < count; i++)
         {
-            VisionTargetInfo target = new VisionTargetInfo(Double.parseDouble(parts[index++]),
-                                       Double.parseDouble(parts[index++]),
-                                       Double.parseDouble(parts[index++]),
-                                       Double.parseDouble(parts[index++]));
-            
-            this.Targets.add(target);
+            Targets.add(new VisionTargetInfo(Double.parseDouble(parts[index++]), Double.parseDouble(parts[index++]), Double.parseDouble(parts[index++]), Double.parseDouble(parts[index++])));
         }
-        
+
     }
 
     /**
@@ -90,17 +74,17 @@ public class VisionMessageTargets
      * Get a decoded/human-readable string
      * @return
      */
-    public String Decoded()
+    public String decoded()
     {
         StringBuilder sb = new StringBuilder(50);
 
-        sb.append("v").append(Version).append(", ");
-        sb.append("Src: ").append(Source).append(", ");
-        sb.append("Img: ").append(ImageWidth).append("x").append(ImageHeight).append(", ");
-        sb.append("FPS: ").append(FPS).append(", ");
-        sb.append("Time: ").append(Timestamp).append(", ");
+        sb.append("v").append(version).append(", ");
+        sb.append("Src: ").append(source).append(", ");
+        sb.append("Img: ").append(imageWidth).append("x").append(imageHeight).append(", ");
+        sb.append("FPS: ").append(fps).append(", ");
+        sb.append("Time: ").append(timestamp).append(", ");
         sb.append("Targets: " + Targets.size());
-        
+
         return sb.toString();
     }
 }
