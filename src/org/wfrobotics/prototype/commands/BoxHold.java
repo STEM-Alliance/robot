72ef0d26bf04fc3623aafa77328dfd6cbdaca606
FRC 4818 The Herd <wfrobotics@gmail.com>
2018-01-18 17:58:23 -0600

    added box hold command renamed stop to intake stop

--- src/org/wfrobotics/prototype/commands/BoxHold.java
+++ src/org/wfrobotics/prototype/commands/BoxHold.java
@@ -1,44 +1,35 @@
 package org.wfrobotics.prototype.commands;
 
-
 import org.wfrobotics.prototype.Robot;
-
 
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class BoxIn extends Command {
-   
-    
-    
-    public BoxIn() {
-  
+public class BoxHold extends Command {
+
+    public BoxHold() {
         requires(Robot.intakeSubsystem);
+     
     }
+
     protected void initialize() {
     }
 
-    
     protected void execute() {
-        double speed = 0.2;
-        Robot.intakeSubsystem.setSpeed(speed); 
-        Robot.intakeSubsystem.holdBox();
+        double speed = 0.1;
+        Robot.intakeSubsystem.setSpeed(speed);
         
     }
-
 
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     }
 }
