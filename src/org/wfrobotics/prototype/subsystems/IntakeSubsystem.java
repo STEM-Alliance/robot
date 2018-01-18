1816835e6bf2c4aa7b5a78bbade4feb4a2915805
wfrobotics <wfrobotics@gmail.com>
2018-01-18 17:34:11 -0600

    added Box detection

--- src/org/wfrobotics/prototype/subsystems/IntakeSubsystem.java
+++ src/org/wfrobotics/prototype/subsystems/IntakeSubsystem.java
@@ -1,26 +1,32 @@
 package org.wfrobotics.prototype.subsystems;
 
-import org.wfrobotics.prototype.commands.Wait;
+import org.wfrobotics.prototype.commands.BoxIn;
+import org.wfrobotics.prototype.commands.Stop;
 
-import com.ctre.CANTalon;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class IntakeSubsystem extends Subsystem {
-    CANTalon RightIntake;
-    CANTalon LeftIntake;
+    TalonSRX RightIntake;
+    TalonSRX LeftIntake;
+    DigitalInput BoxCheck;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     public IntakeSubsystem()
     {
-      RightIntake = new CANTalon(19);
-      LeftIntake = new CANTalon(20);
+      RightIntake = new TalonSRX(19);
+      LeftIntake = new TalonSRX(20);
       RightIntake.setInverted(true);
       LeftIntake.set(ControlMode.Follower, 1);
+      BoxCheck = new DigitalInput(0);
         // TODO Auto-generated constructor stub
     }
     
@@ -28,7 +34,7 @@ public class IntakeSubsystem extends Subsystem {
          return false;
      }
     public void initDefaultCommand() {
-        setDefaultCommand(new Wait());
+        setDefaultCommand(new Stop());
         
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
@@ -38,7 +44,18 @@ public class IntakeSubsystem extends Subsystem {
        RightIntake.set(ControlMode.PercentOutput, rpm);
       
     }
-  
+    public boolean CheckBox(){
+        return BoxCheck.get();
+      }
+    public void HoldBox(){
+        if (CheckBox() == true)
+        {
+            setDefaultCommand(new BoxIn());
+        }
+        else {
+            setDefaultCommand(new Stop());
+        }
+    }
     
 }
 
