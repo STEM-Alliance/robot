6fe0776540e889fc114d21615bea4aa48dd727a4
David Lindner <David Lindner@localhost>
2018-01-21 08:20:58 -0600

    Initial - Constant acceleration motor control

--- src/org/wfrobotics/prototype/commands/DriveStop.java
+++ src/org/wfrobotics/prototype/commands/DriveStop.java
@@ -5,18 +5,16 @@ import org.wfrobotics.prototype.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 /** Sets ExampleSubsystem to a safe state */
-public class ExampleStopCommand extends Command
+public class DriveStop extends Command
 {
-    public ExampleStopCommand()
+    public DriveStop()
     {
         requires(Robot.prototypeSubsystem);
     }
 
     protected void execute()
     {
-        int rpm = 0;
-
-        Robot.prototypeSubsystem.setSpeed(rpm);
+        Robot.prototypeSubsystem.doMagic(0);
     }
 
     protected boolean isFinished()
--- src/org/wfrobotics/prototype/config/IO.java
+++ src/org/wfrobotics/prototype/config/IO.java
@@ -1,6 +1,6 @@
 package org.wfrobotics.prototype.config;
 
-import org.wfrobotics.prototype.commands.ExampleForwardCommand;
+import org.wfrobotics.prototype.commands.DriveConstAccel;
 import org.wfrobotics.reuse.controller.ButtonFactory;
 import org.wfrobotics.reuse.controller.ButtonFactory.TRIGGER;
 import org.wfrobotics.reuse.controller.Xbox;
@@ -12,8 +12,8 @@ public class IO
 {
     public static Xbox controller = new Xbox(0);
 
-    public static Button X = ButtonFactory.makeButton(controller, Xbox.BUTTON.X, TRIGGER.WHILE_HELD, new ExampleForwardCommand());
-    public static Button Y = ButtonFactory.makeButton(controller, Xbox.BUTTON.Y, TRIGGER.WHILE_HELD, new ExampleForwardCommand());
+    public static Button X = ButtonFactory.makeButton(controller, Xbox.BUTTON.X, TRIGGER.WHILE_HELD, new DriveConstAccel());
+    public static Button Y = ButtonFactory.makeButton(controller, Xbox.BUTTON.Y, TRIGGER.WHILE_HELD, new DriveConstAccel());
     // TODO Switch the 'Y' Button to run another Command, such as ExampleBackwardCommand
     // TODO create more buttons if you need them to do your testing
 }
