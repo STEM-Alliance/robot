c8b9b81b0dd54a70b84fb4a06705ccc6a43beba4
David Lindner <lindnerdavidr@johndeere.com>
2018-01-25 00:53:14 -0600

    Make game specific auto mode cleaner, fix array indent craziness

--- src/org/wfrobotics/robot/config/Autonomous.java
+++ src/org/wfrobotics/robot/config/Autonomous.java
@@ -1,90 +1,63 @@
 package org.wfrobotics.robot.config;
 
-import java.util.function.Supplier;
-
 import org.wfrobotics.reuse.commands.drivebasic.AutoDrive;
 import org.wfrobotics.reuse.commands.drivebasic.DriveOff;
-import org.wfrobotics.reuse.hardware.sensors.Gyro;
+import org.wfrobotics.reuse.utilities.AutoPicker;
 import org.wfrobotics.reuse.utilities.HerdLogger;
 import org.wfrobotics.reuse.utilities.HerdVector;
 import org.wfrobotics.robot.Robot;
-import org.wfrobotics.robot.RobotState;
 
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 // TODO Recursively log mode/profile of composing commands with newlines, could ask each to print their toString?
 
-public class Autonomous
+/** Organizes autonomous modes supported by Robot **/
+public class Autonomous extends AutoPicker
 {
-    static final double DRIVE_SPEED = .6;
-    static final double TIME_DRIVE_MODE = 4;
+    private enum POSITION_ROTARY {RIGHT, CENTER, LEFT};
 
-    public enum POSITION_ROTARY {SIDE_A, CENTER, SIDE_B};
-    public static class AutoMode
+    private final double DRIVE_SPEED = .6;
+    private final double TIME_DRIVE_MODE = 4;
+
+    /** FIRST Power Up - Top level autonomous modes **/
+    protected AutoMode[] makeModes()
     {
-        public final String text;
-        public final Supplier<Command> maker;
-        public final double gyroOffset;
-
-        public AutoMode(String chooserText, Supplier<Command> mode, double gyro)
-        {
-            text = chooserText;
-            maker = mode;
-            gyroOffset = gyro;
-        }
-    }
-
-    private static AutoMode[] makeModes()
-    {
-        getRotaryStartingPosition();
-        DriverStation.getInstance().getAlliance();
+        // TODO Intelligently use the starting position from panel board dial? Which commands care where we start the robot?
 
         return new AutoMode[] {
-                new AutoMode("Auto None", () -> new DriveOff(), 0),
-                new AutoMode("Auto Forward (LOW GEAR)", () -> new AutoDrive(new HerdVector(DRIVE_SPEED, 0), TIME_DRIVE_MODE), 0),
-                new AutoMode("Auto Forward (HIGH GEAR)", () -> new AutoDrive(new HerdVector(DRIVE_SPEED * .75, 0), TIME_DRIVE_MODE * .75), 0),
+            new AutoMode("Auto None", () -> new DriveOff(), 0),
+            // TODO Delete this if cross line command works?
+            new AutoMode("Auto Forward (LOW GEAR)", () -> new AutoDrive(new HerdVector(DRIVE_SPEED, 0), TIME_DRIVE_MODE), 0),
+            new AutoMode("Auto Forward (HIGH GEAR)", () -> new AutoDrive(new HerdVector(DRIVE_SPEED * .75, 0), TIME_DRIVE_MODE * .75), 0),
+            // TODO Drive distance, cross line - must have by first regional
+            // TODO Score on scale - PRIORITIZE MAKING THIS AMAZING!!!
+            // TODO Score on scale then do some extra stuff(s)
+            // TODO Score switch first
         };
     }
 
-    public static Command setupSelectedMode()
+    /** Grabs the selected 'Auto Mode' from SmartDashboard, sets up gyro, returns the command to run in autonomous */
+    public static Command setupAndReturnSelectedMode()
     {
-        String selected = SmartDashboard.getString("Auto Mode", "Auto None");
-        AutoMode[] modes = makeModes();
-        int choice = 0;
-
-        for (int index = 0; index < modes.length; index++)
-        {
-            if (modes[index].text.equals(selected)) { choice = index; break; }
-        }
-
-        Gyro.getInstance().zeroYaw(modes[choice].gyroOffset);
-        RobotState.getInstance().updateRobotHeading(modes[choice].gyroOffset);
-        return modes[choice].maker.get();
+        return new Autonomous().setupSelectedMode();  // Static getter seemed cleaner in Robot
     }
 
-    /**
-     * Get the starting position on field for when we switch to autonomous mode
-     * @return Boiler, Center, or Loading Station side of field
-     */
-    public static POSITION_ROTARY getRotaryStartingPosition()
+    /** Get the starting position on field for when we switch to autonomous mode */
+    protected static POSITION_ROTARY getRotaryStartingPosition()
     {
         HerdLogger log = new HerdLogger(POSITION_ROTARY.class);
         int dial = Robot.controls.getAutonomousSide();
-        Alliance alliance = DriverStation.getInstance().getAlliance();
         POSITION_ROTARY position;
 
-        if (alliance == Alliance.Blue && dial == 7 || alliance == Alliance.Red && dial == 1)
+        if (dial == 1)
         {
-            position = POSITION_ROTARY.SIDE_A;
-            log.info("Autonomous Starting Position", "BOILER");
+            position = POSITION_ROTARY.RIGHT;
+            log.info("Autonomous Starting Position", "RIGHT");
         }
-        else if (alliance == Alliance.Blue && dial == 1 || alliance == Alliance.Red && dial == 7)
+        else if (dial == 7)
         {
-            position = POSITION_ROTARY.SIDE_B;
-            log.info("Autonomous Starting Position", "LOADING STATION");
+            position = POSITION_ROTARY.LEFT;
+            log.info("Autonomous Starting Position", "LEFT");
         }
         else if (dial == 0)
         {
