c45e2e5dc6b78cd41055dceb72e0779a3a0957ff
David Lindner <David Lindner@localhost>
2018-02-05 06:57:50 -0600

    Very improved due to tiny I, good sync-ness measuring

---
+++ src/org/wfrobotics/robot/commands/LiftPercentVoltage.java
@@ -0,0 +1,40 @@
+package org.wfrobotics.robot.commands;
+
+import org.wfrobotics.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class LiftPercentVoltage extends Command
+{
+    private final double deadbandPercent = .2;
+
+    public LiftPercentVoltage()
+    {
+        requires(Robot.liftSubsystem);
+    }
+
+    protected void initialize()
+    {
+        SmartDashboard.putString("Lift", getClass().getSimpleName());
+    }
+
+    protected void execute()
+    {
+        double setpoint = Robot.controls.getLiftPercent();
+
+        if (Math.abs(setpoint) < deadbandPercent)
+        {
+            Robot.liftSubsystem.goToSpeedInit(0);
+        }
+        else
+        {
+            Robot.liftSubsystem.goToSpeedInit(setpoint);
+        }
+    }
+
+    protected boolean isFinished()
+    {
+        return false;
+    }
+}
