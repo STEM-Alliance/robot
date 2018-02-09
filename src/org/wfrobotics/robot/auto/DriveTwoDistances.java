7d763262ac431f85bf508308fa1c8f3c31fdbbba
FRC 4818 The Herd <wfrobotics@gmail.com>
2018-02-08 19:42:20 -0600

    added logic to commands to allow for the robot to use TurnMagic

--- src/org/wfrobotics/robot/auto/DriveTwoDistances.java
+++ src/org/wfrobotics/robot/auto/DriveTwoDistances.java
@@ -9,8 +9,13 @@ public class DriveTwoDistances extends CommandGroup
 {
     public DriveTwoDistances()
     {
-        this.addSequential(new DriveDistance(12));
+        this.addSequential(new DriveDistance(12*19+0));
         this.addSequential(new TurnToHeading(-90,1));
-        this.addSequential(new DriveDistance(12));
+        this.addSequential(new DriveDistance(19.0 * 12.0));
+        this.addSequential(new TurnToHeading(-180,1));
+        this.addSequential(new DriveDistance(14 * 12));
+        this.addSequential(new TurnToHeading(90,1));
+        this.addSequential(new DriveDistance(213));
+        this.addSequential(new TurnToHeading(0,1));
     }
 }
