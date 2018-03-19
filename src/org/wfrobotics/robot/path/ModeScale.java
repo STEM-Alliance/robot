729b349b8048d62baa3b2051084188a7e25f3dbd
Kyle Schmtiz <Kyle Schmtiz@localhost>
2018-03-18 21:44:03 -0500

    temp commit for auto stuff

--- src/org/wfrobotics/robot/path/ModeScale.java
+++ src/org/wfrobotics/robot/path/ModeScale.java
@@ -18,7 +18,7 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class ModeScale extends CommandGroup
 {
-    private final double inchesToSecondCube = 12.0 * 4.0;
+    private final double inchesToSecondCube = 88;
     private final double speedOuttake = 1.0;//0.55;
     private final double timeOuttake = 0.5;
     private final double waitForGyroToFullyZero = Double.MIN_VALUE;
@@ -30,7 +30,7 @@ public class ModeScale extends CommandGroup
         addSequential(new SynchronizedCommand(new DriveDistance(12.0 * 24.5 - 33.6), new DelayedCommand(new LiftToScale(), 1.75)));
         addSequential(new TurnToHeading(-30.0, 1.0));
         addSequential(new IntakeSet(speedOuttake, timeOuttake, true));
-        addSequential(new TurnToHeading(-175.0, 1.0));
+        addSequential(new TurnToHeading(-145.06, 1.0)); // to find distance: x= 51 y= 73
         addSequential(new LiftToHeight(LiftHeight.Intake.get()));  // TODO ScaleChoice
         addParallel(new LiftGoHome(-0.2, 0.5));  // Ensure at smart intake height
         addParallel(new JawsSet(true, 0.1, false));  // Prime smart intake
@@ -38,10 +38,11 @@ public class ModeScale extends CommandGroup
         //        addSequential(new DriveDistance(inchesToSecondCube));
         //        addSequential(new DriveDistance(12.0 * 3.0));
         nom();
+
         addParallel(new IntakeLiftToHeight(1.0));
-        addSequential(new TurnToHeading(0.0, 1.0));  // TODO ScaleChoice
+        addSequential(new TurnToHeading(30.0, 1.0));  // TODO ScaleChoice
         addSequential(new SynchronizedCommand(new DriveDistance(inchesToSecondCube), new DelayedCommand(new LiftToScale(), 0.0)));
-        addSequential(new TurnToHeading(-20.0, 1.0));  // TODO ScaleChoice
+        addSequential(new TurnToHeading(-30.0, 1.0));  // TODO ScaleChoice
         addSequential(new IntakeSet(speedOuttake * 0.8, timeOuttake, true));
     }
 
@@ -49,6 +50,6 @@ public class ModeScale extends CommandGroup
     {
         addParallel(new JawsSet(true, 0.1, false));  // Prime smart intake
         addParallel(new SmartIntake());
-        addSequential(new DriveInfared(6.0, 1.5));
+        addSequential(new DriveDistance(89));
     }
 }
