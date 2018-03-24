744dfa17628726fa832ce08550ee15f46280d948
FRC 4818 The Herd <wfrobotics@gmail.com>
2018-03-24 13:32:16 -0500

    after qual 1

--- src/org/wfrobotics/robot/path/ModeCenter.java
+++ src/org/wfrobotics/robot/path/ModeCenter.java
@@ -6,9 +6,11 @@ import org.wfrobotics.reuse.commands.drive.DrivePath;
 import org.wfrobotics.reuse.commands.drive.TurnToHeading;
 import org.wfrobotics.reuse.utilities.MatchState2018.Side;
 import org.wfrobotics.robot.auto.IntakeSet;
+import org.wfrobotics.robot.commands.intake.SmartIntake;
 import org.wfrobotics.robot.commands.wrist.WristToHeight;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class ModeCenter extends CommandGroup
 {
@@ -17,7 +19,33 @@ public class ModeCenter extends CommandGroup
         addParallel(new WristToHeight(1.0));
         addSequential(new SwitchChoice(Side.Right, new DrivePath("CenterRight"), new DrivePath("CenterLeft")));
         addSequential(new IntakeSet(1.0, 0.5, true));  // Yes, 1.0 outtake is good here
+
+        oneCube();
+        //        twoCube();
+    }
+
+    public void oneCube()
+    {
+        // Get away from wall
         addSequential(new DriveDistance(-12.0 * 1.0));
-        addParallel(new SwitchChoice(Side.Right, new TurnToHeading(-90.0, 1.5), new TurnToHeading(90.0, 1.5)));
+        addParallel(new WristToHeight(0.0));
+        addSequential(new SwitchChoice(Side.Right, new TurnToHeading(-90.0, 1.5), new TurnToHeading(90.0, 1.5)));
+    }
+
+    public void twoCube()
+    {
+        // Get away from wall
+        addParallel(new WristToHeight(0.0));
+        addSequential(new DriveDistance(-12.0 * 8.5));
+
+        // Picking up secound cube
+        addParallel(new SmartIntake());
+        addSequential(new SwitchChoice(Side.Right, new DrivePath("CenterRightSecound"), new WaitCommand(3.0)));
+        addSequential(new WaitCommand(.1));
+
+        // Third path to the switch
+        addSequential(new SwitchChoice(Side.Right, new DrivePath("CenterRightThird"), new WaitCommand(3.0)));
+        addSequential(new IntakeSet(1.0, 0.5, true));
+        addSequential(new SwitchChoice(Side.Right, new TurnToHeading(90.0, 2.0), new TurnToHeading(-90.0, 2.0)));
     }
 }
