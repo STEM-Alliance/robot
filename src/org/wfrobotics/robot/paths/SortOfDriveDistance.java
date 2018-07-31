8c9cd9c57b0df9ee211ab19f60b90985de76dc84
David Lindner <lindnerdavidr@johndeere.com>
2018-07-31 08:33:19 -0500

    Use unit test path

--- src/org/wfrobotics/robot/paths/SortOfDriveDistance.java
+++ src/org/wfrobotics/robot/paths/SortOfDriveDistance.java
@@ -3,20 +3,23 @@ package org.wfrobotics.robot.paths;
 import java.util.ArrayList;
 
 import org.wfrobotics.reuse.config.PathBuilder;
-import org.wfrobotics.reuse.config.PathContainer;
 import org.wfrobotics.reuse.config.PathBuilder.Waypoint;
+import org.wfrobotics.reuse.config.PathContainer;
 import org.wfrobotics.reuse.math.rigidtransform.RigidTransform2d;
 import org.wfrobotics.reuse.math.rigidtransform.Rotation2d;
 import org.wfrobotics.reuse.math.rigidtransform.Translation2d;
 import org.wfrobotics.reuse.subsystems.control.PathAdaptive;
+import org.wfrobotics.reuse.subsystems.drive.TankMaths;
+import org.wfrobotics.robot.config.robotConfigs.RobotConfig;
 
 public class SortOfDriveDistance implements PathContainer
 {
     public PathAdaptive buildPath()
     {
+        double velocity = TankMaths.ticksToInchesPerSecond(RobotConfig.getInstance().getTankConfig().VELOCITY_MAX);
         ArrayList<Waypoint> sWaypoints = new ArrayList<Waypoint>();
         sWaypoints.add(new Waypoint(0, 0, 0, 0));
-        sWaypoints.add(new Waypoint(0, 17 * 12, 0, 40));
+        sWaypoints.add(new Waypoint(17 * 12, 0, 0, velocity));
 
         return PathBuilder.buildPathFromWaypoints(sWaypoints);
     }
