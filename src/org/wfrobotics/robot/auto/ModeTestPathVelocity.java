b27d6f23b0f6e5a3f1f0c29c43169e466e99548e
David Lindner <lindnerdavidr@johndeere.com>
2018-08-07 08:13:13 -0500

    Path auto group, resets starting position

--- src/org/wfrobotics/robot/auto/ModeTestPathVelocity.java
+++ src/org/wfrobotics/robot/auto/ModeTestPathVelocity.java
@@ -1,6 +1,9 @@
 package org.wfrobotics.robot.auto;
 
-import org.wfrobotics.reuse.commands.drive.DrivePathVelocity;
+import org.wfrobotics.reuse.commands.drive.DrivePath;
+import org.wfrobotics.reuse.commands.driveconfig.ResetPose;
+import org.wfrobotics.reuse.config.PathContainer;
+import org.wfrobotics.robot.paths.SortOfDriveDistance;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
@@ -9,7 +12,10 @@ public class ModeTestPathVelocity extends CommandGroup
 {
     public ModeTestPathVelocity()
     {
-        this.addSequential(new DrivePathVelocity(12.0 * 17.0, 12.0 * 0.0));
+        PathContainer path = new SortOfDriveDistance();
+
+        this.addSequential(new ResetPose(path));
+        this.addSequential(new DrivePath(path));
         this.addSequential(new WaitCommand(2.0));
     }
 }
