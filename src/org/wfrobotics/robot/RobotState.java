24845331715bdc41c7f5a0341219dc2a158450ca
Troy Mullenberg <23370494+VoidedIceberg@users.noreply.github.com>
2019-01-04 11:18:27 -0600

    Cleaned out for 2k19 #DeepSpace

--- src/org/wfrobotics/robot/RobotState.java
+++ src/org/wfrobotics/robot/RobotState.java
@@ -5,12 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.wfrobotics.reuse.RobotStateBase;
-import org.wfrobotics.reuse.subsystems.vision.CameraServer;
 import org.wfrobotics.reuse.subsystems.vision.CoprocessorData;
 import org.wfrobotics.reuse.subsystems.vision.CoprocessorData.VisionTargetInfo;
 import org.wfrobotics.reuse.subsystems.vision.Point;
-import org.wfrobotics.robot.config.IO;
-import org.wfrobotics.robot.config.RobotConfig;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -18,26 +15,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /** Preferred provider of global, formatted state about the robot. Commands can get information from one place rather than from multiple subsystems. **/
 public final class RobotState extends RobotStateBase
 {
-    private static final int kHasCubeCountThreshold = 20;
-    private final double kIntakeDistanceHasCube;
-    private static final double kHasCubeSignalDriveTeamDuration = 2.0;
-
     private static final RobotState instance = new RobotState();
-    private int hasCubeCounts;
-    private double timeSinceRumbleOn;
-
-    public final double kcameraAngle = 34.5;
-
-    // Robot-specific state
-    public boolean robotHasCube;
-    public CoprocessorData update;
-    public  List<Point> points = new ArrayList<Point>();
-    public boolean visionInView = false;
-
 
     public RobotState()
     {
-        kIntakeDistanceHasCube = RobotConfig.getInstance().kIntakeDistanceToCube;
+
     }
 
     public static RobotState getInstance()
@@ -48,27 +30,28 @@ public final class RobotState extends RobotStateBase
     public void reportState()
     {
         super.reportState();
-        SmartDashboard.putBoolean("Has Cube", robotHasCube);
-        SmartDashboard.putString("Reported State", "True");
-        SmartDashboard.putNumber("target", points.size());
-        try
-        {
-            SmartDashboard.putBoolean("Has VisionServer", (CameraServer.getInstance() != null));
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        SmartDashboard.putBoolean("Target In View", visionInView);
 
     }
 
     protected synchronized void resetRobotSpecificState()
     {
-        robotHasCube = false;
-        hasCubeCounts = 0;
 
     }
+
+
+    /**    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+     *     |             Now entering Vision code territory                         |
+     *     |   WF robotic is not responsible for anything cased by confusion!       |
+     *     |________________________________________________________________________|
+     */
+
+    public final double kcameraAngle = 34.5;
+
+    // Robot-specific state
+    public CoprocessorData update;
+    public  List<Point> points = new ArrayList<Point>();
+    public boolean visionInView = false;
+
     public void addVisionUpdate(Double time, CoprocessorData coprocessorData)
     {
         SmartDashboard.putString("Ran vision update", "Ran Vision Update");
@@ -105,46 +88,5 @@ public final class RobotState extends RobotStateBase
     public double getVisionError()
     {
         return points.get(0).getXerror();
-    }
-    public void updateIntake(double distance)
-    {
-        handleDetectCube(distance);
-
-        if(robotHasCube)
-        {
-            final double now = Timer.getFPGATimestamp();
-
-            IO.getInstance().setRumble(now - timeSinceRumbleOn < 1.0);
-            if (now - timeSinceRumbleOn < kHasCubeSignalDriveTeamDuration)
-            {
-                Robot.leds.signalDriveTeam();  // TODO latched boolean?
-            }
-            else
-            {
-                Robot.leds.useRobotModeColor();
-                timeSinceRumbleOn = 0.0;
-            }
-        }
-        else
-        {
-            IO.getInstance().setRumble(false);
-        }
-    }
-
-    private synchronized void handleDetectCube(double cubeDistanceInches)
-    {
-        if (cubeDistanceInches < kIntakeDistanceHasCube)
-        {
-            hasCubeCounts++;
-        }
-        else if (hasCubeCounts < kHasCubeCountThreshold)
-        {
-            timeSinceRumbleOn = Timer.getFPGATimestamp();
-        }
-        else
-        {
-            hasCubeCounts = 0;
-        }
-        robotHasCube = hasCubeCounts > kHasCubeCountThreshold;
     }
 }
--- src/org/wfrobotics/robot/config/IO.java
+++ src/org/wfrobotics/robot/config/IO.java
@@ -1,20 +1,12 @@
 package org.wfrobotics.robot.config;
 
 import org.wfrobotics.reuse.commands.SignalHumanPlayer;
-import org.wfrobotics.reuse.commands.drive.TurnToHeading;
 import org.wfrobotics.reuse.commands.drive.TurnToTarget;
 import org.wfrobotics.reuse.config.AutoFactory;
 import org.wfrobotics.reuse.config.ButtonFactory;
 import org.wfrobotics.reuse.config.ButtonFactory.TRIGGER;
 import org.wfrobotics.reuse.config.HerdJoystick;
 import org.wfrobotics.reuse.config.Xbox;
-import org.wfrobotics.reuse.config.Xbox.DPAD;
-import org.wfrobotics.robot.auto.ModeScale;
-import org.wfrobotics.robot.commands.AutoLiftToBottom;
-import org.wfrobotics.robot.commands.AutoLiftToScale;
-import org.wfrobotics.robot.commands.intake.IntakeManual;
-import org.wfrobotics.robot.commands.intake.JawsToggle;
-import org.wfrobotics.robot.commands.intake.SmartOutake;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.Joystick;
@@ -44,80 +36,24 @@ public final class IO
         ButtonFactory.makeButton(driverThrottle, HerdJoystick.BUTTON.BASE_MIDDLE_RIGHT, TRIGGER.WHEN_PRESSED, AutoFactory.getInstance().makeCommand(Auto.delays));
         ButtonFactory.makeButton(driverThrottle, HerdJoystick.BUTTON.BASE_BOTTOM_RIGHT, TRIGGER.WHEN_PRESSED, AutoFactory.getInstance().makeCommand(Auto.positions));
 
-        // ------------------------- Drive ------------------------
-
-        // ------------------------ Intake ------------------------
-
-        ButtonFactory.makeButton(operator, Xbox.AXIS.RIGHT_TRIGGER, .1, TRIGGER.WHILE_HELD, new IntakeManual());
-        ButtonFactory.makeButton(operator, Xbox.AXIS.LEFT_TRIGGER, .1, TRIGGER.WHILE_HELD, new IntakeManual());
-        ButtonFactory.makeButton(operator, Xbox.BUTTON.LB, TRIGGER.WHEN_PRESSED, new JawsToggle());
-        ButtonFactory.makeButton(operator, Xbox.BUTTON.B, TRIGGER.WHEN_PRESSED, new SmartOutake());
-
-        // ------------------------- Lift -------------------------
-
-        ButtonFactory.makeButton(operator, Xbox.BUTTON.Y, TRIGGER.WHEN_PRESSED, new AutoLiftToScale());
-        ButtonFactory.makeButton(operator, Xbox.BUTTON.A, TRIGGER.WHEN_PRESSED, new AutoLiftToBottom());
+        //----------------------Testing-------------------------------
         ButtonFactory.makeButton(operator, Xbox.BUTTON.X, TRIGGER.WHEN_PRESSED, new TurnToTarget(10));
 
         // -------------------- Super Structure -------------------
 
         ButtonFactory.makeButton(operator, Xbox.DPAD.RIGHT, TRIGGER.WHILE_HELD, new SignalHumanPlayer());
 
-        // ------------------------ Debug -------------------------
-
-        //        ButtonFactory.makeButton(operator, Xbox.BUTTON.START, TRIGGER.WHEN_PRESSED, new ModeTestPathVelocity());
-        //        ButtonFactory.makeButton(operator, Xbox.BUTTON.BACK, TRIGGER.WHEN_PRESSED, new DrivePathTest(17.0 * 12.0, 0.0 * 12.0));
-        ButtonFactory.makeButton(operator, Xbox.BUTTON.START, TRIGGER.WHEN_PRESSED, new ModeScale());
-        //        ButtonFactory.makeButton(operator, Xbox.BUTTON.BACK, TRIGGER.WHEN_PRESSED, new ModeOppisitScalse());
-        ButtonFactory.makeButton(operator, Xbox.BUTTON.BACK, TRIGGER.WHEN_PRESSED, new TurnToHeading(130.0));
-        //        ButtonFactory.makeButton(operator, Xbox.BUTTON.BACK, TRIGGER.WHEN_PRESSED, new TuningTrajectory());
-        //        ButtonFactory.makeButton(operator, Xbox.BUTTON.BACK, TRIGGER.TOGGLE_WHEN_PRESSED, new BlinkInArea(60.0, 36.0, 36.0, 60.0));
-        //        ButtonFactory.makeButton(operator, Xbox.BUTTON.START, TRIGGER.WHEN_PRESSED, new CharacterizeDrivetrain());
     }
 
     // ------------------- Robot-specific --------------------
 
-    public double getIntakeIn()
-    {
-        return operator.getTrigger(Hand.kRight);
-    }
 
-    public double getIntakeOut()
-    {
-        return operator.getTrigger(Hand.kLeft);
-    }
 
-    public double getWristStick()
-    {
-        return operator.getAxis(Xbox.AXIS.LEFT_Y);
-    }
+    //    public double getWristStick()
+    //    {
+    //        return operator.getAxis(Xbox.AXIS.LEFT_Y);
+    //    }
 
-    public double getLiftStick()
-    {
-        return operator.getAxis(Xbox.AXIS.RIGHT_Y);
-    }
-
-    public double getWinchPercent()
-    {
-        int val = operator.getDpad();
-        double speed = 0.0;
-
-        if (val == DPAD.UP.get() || val == DPAD.UP_LEFT.get() || val == DPAD.UP_RIGHT.get())
-        {
-            speed = 1.0;
-        }
-        else if (val == DPAD.DOWN.get() || val == DPAD.DOWN_LEFT.get() || val == DPAD.DOWN_RIGHT.get())
-        {
-            speed = -1.0;
-        }
-
-        return speed;
-    }
-
-    public boolean isLiftOverrideRequested()
-    {
-        return  Math.abs(getLiftStick()) > .15;
-    }
 
     // ------------------------ Reuse ------------------------
 
