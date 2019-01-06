3afa714ffa2bdae2be3057ed7742698fb28f0a88
Troy Mullenberg <23370494+VoidedIceberg@users.noreply.github.com>
2019-01-05 19:19:23 -0600

    Compiling default

--- src/org/wfrobotics/robot/subsystems/SuperStructure.java
+++ src/org/wfrobotics/robot/subsystems/SuperStructure.java
@@ -1,7 +1,6 @@
 package org.wfrobotics.robot.subsystems;
 
 import org.wfrobotics.reuse.subsystems.SuperStructureBase;
-import org.wfrobotics.robot.commands.ConserveCompressor;
 
 public class SuperStructure extends SuperStructureBase
 {
@@ -17,6 +16,6 @@ public class SuperStructure extends SuperStructureBase
 
     protected void initDefaultCommand()
     {
-        setDefaultCommand(new ConserveCompressor());
+
     }
 }
