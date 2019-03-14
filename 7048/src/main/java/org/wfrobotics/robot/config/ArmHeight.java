65548d9508e702adf2a2888bde4d6b3ddfe21aed
RedRiverRage <redriverrage@gmail.com>
2019-03-14 18:33:06 -0500

    practice day

--- 7048/src/main/java/org/wfrobotics/robot/config/ArmHeight.java
+++ 7048/src/main/java/org/wfrobotics/robot/config/ArmHeight.java
@@ -3,10 +3,10 @@ package org.wfrobotics.robot.config;
 /** All heights are relative distances the lift must move from bottom limit switch */
 public enum ArmHeight
 {
-    TopLimit(37.0),   // TODO Tune
-    HatchHigh(36.0),   // TODO Tune or remove
-    HatchMiddle(25.00),  // TODO Tune or remove
-    HatchLow(24.00),  // TODO Tune or remove
+    TopLimit(49.0),   // TODO Tune
+    HatchHigh(49.0),   // TODO Tune or remove
+    HatchMiddle(34.6),  // TODO Tune or remove
+    HatchLow(12.0),  // TODO Tune or remove
     BottomLimit(12.00);  // TODO Tune
 
     private final double value;
