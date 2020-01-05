77e6247e395eda2ddb11c0462fda73c5ffe1c4fd
Troy Mullenberg <tmullenberg@hotmail.com>
2020-01-04 21:02:11 -0600

    Filled in version of the "Prototype tutorial" (Will undo this once we can verify that it builds and deploys without crashing on the rio) Also included a slightly striped version of 4818 THE HERD'S network table vision code

--- prototype/src/main/java/org/wfrobotics/robot/commands/ExampleStopCommand.java
+++ prototype/src/main/java/org/wfrobotics/robot/commands/ExampleStopCommand.java
@@ -1,22 +1,23 @@
 package org.wfrobotics.robot.commands;
 
-import org.wfrobotics.robot.Robot;
+import org.wfrobotics.robot.subsystems.ExampleSubsystem;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 /** Sets ExampleSubsystem to a safe state */
 public class ExampleStopCommand extends Command
 {
+    ExampleSubsystem exampleSubsystem = ExampleSubsystem.getInstance();
     public ExampleStopCommand()
     {
-        requires(Robot.prototypeSubsystem);
+        requires(exampleSubsystem);
     }
 
     protected void execute()
     {
-        int percentForward = 0;
+        double percentForward = 0.0;
 
-        Robot.prototypeSubsystem.setSpeed(percentForward);
+        ExampleSubsystem.getInstance().setSpeed(percentForward);
     }
 
     protected boolean isFinished()
--- prototype/src/main/java/org/wfrobotics/robot/subsystems/ExampleSubsystem.java
+++ prototype/src/main/java/org/wfrobotics/robot/subsystems/ExampleSubsystem.java
@@ -1,7 +1,11 @@
 package org.wfrobotics.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import org.wfrobotics.reuse.subsystems.EnhancedSubsystem;
 import org.wfrobotics.robot.commands.ExampleStopCommand;
+
 
 /** TODO: comment what this Subsystem does */
 // TODO rename me (right click -> refactor -> rename)
@@ -9,9 +13,12 @@ public class ExampleSubsystem extends EnhancedSubsystem
 {
     // TODO create any hardware this Subsystem needs to control the robot
 
+    TalonSRX motor1;
+
     public ExampleSubsystem()
     {
         // TODO setup any hardware this Subsystem will need
+        motor1 = new TalonSRX(18);
     }
 
     protected void initDefaultCommand()
@@ -20,6 +27,12 @@ public class ExampleSubsystem extends EnhancedSubsystem
         setDefaultCommand(new ExampleStopCommand());
     }
 
+    // TODO rename me (right click -> refactor -> rename)
+    public void setSpeed(double percentForward)  // TODO pass any parameters needed to each method
+    {
+         motor1.set(ControlMode.PercentOutput, Math.abs(percentForward));
+    }
+    
     public void cacheSensors(boolean isDisabled)
     {
 
@@ -27,17 +40,27 @@ public class ExampleSubsystem extends EnhancedSubsystem
 
     public void reportState()
     {
-
-    }
-
-    // TODO rename me (right click -> refactor -> rename)
-    public void setSpeed(int percentForward)  // TODO pass any parameters needed to each method
-    {
-        // TODO create any methods needed to control this Subsystem, called by Commands
+        
     }
 
     public TestReport runFunctionalTest()
     {
         return new TestReport();
+    }
+    
+    /**
+     * 
+     *  The code below is the control that needs to be put in every subsystem to ensure that there 
+     *  is only one version of the subsystem running on the robot at one time!
+     * 
+     */
+    static class SingletonHolder
+    {
+        static ExampleSubsystem instance = new ExampleSubsystem();
+    }
+
+    public static ExampleSubsystem getInstance()
+    {
+        return SingletonHolder.instance;
     }
 }
