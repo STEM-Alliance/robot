0711744371ad2173113f72ff45116596eadc2055
Troy Mullenberg <tmullenberg@hotmail.com>
2020-01-07 15:34:25 -0600

    2020 upgrade...... RIP old project structure..... Hellow new way (the old way) of doing things **TL;DR** -WPI-lib broke a lot of thing in our reuse repo having to do with making ocmmands and subsystems fancyier -I tryed to salvage all that I could -It's not as bad as it seams...... trust me.....

---
+++ 4360/src/main/java/frc/robot/reuse/hardware/sensors/MagnetoPotAnalog.java
@@ -0,0 +1,55 @@
+package frc.robot.reuse.hardware.sensors;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+
+/**
+ * Class to use a Magnetic Potentiometer like an analog
+ * potentiometer, when it doesn't have the full range of 0 to 1.
+ * Created for using the 6127V1A360L.5FS (987-1393-ND on DigiKey)
+ */
+public class MagnetoPotAnalog extends MagnetoPot
+{
+    public AnalogPotentiometer m_Analog;
+
+    /**
+     * Initialize a new Magnetic Potentiometer
+     * @param channel Analog input channel
+     */
+    public MagnetoPotAnalog(int channel)
+    {
+        this(channel, 1);
+    }
+
+    /**
+     * Initialize a new Magnetic Potentiometer
+     * @param channel Analog input channel
+     * @param fullRange full range to scale output to (360 would give output of 0-360)
+     */
+    public MagnetoPotAnalog(int channel, double fullRange)
+    {
+        this(channel, fullRange, 0);
+    }
+
+    /**
+     * Initialize a new Magnetic Potentiometer
+     * @param channel Analog input channel
+     * @param fullRange full range to scale output to (360 would give output of 0-360)
+     * @param offset offset to scale output to (180 would give output of 180-360)
+     */
+    public MagnetoPotAnalog(int channel, double fullRange, double offset)
+    {
+        super(fullRange, offset);
+
+        m_Analog = new AnalogPotentiometer(channel);
+    }
+
+    public double getRawInput()
+    {
+        return m_Analog.get();
+    }
+
+    @Override
+    public void free() {
+
+    }
+}
---
+++ 4360/src/main/java/frc/robot/reuse/math/WrappedAngle.java
@@ -0,0 +1,10 @@
+package frc.robot.reuse.math;
+
+/** Wrapped angles support their own range in degrees, agreeing to "wrap" all values back into this range automatically */
+public interface WrappedAngle
+{
+    public double getAngle();                           // Degrees
+    public WrappedAngle rotate(double angle);           // Positive: Rotate clockwise by angle     Negative: Rotate counterclockwise by angle
+    public WrappedAngle rotate(WrappedAngle b);         // Positive: Rotate clockwise by b         Negative: Rotate counterclockwise by b
+    public WrappedAngle rotateReverse(WrappedAngle b);  // Positive: Rotate counterclockwise by b  Negative: Rotate clockwise by b
+}
