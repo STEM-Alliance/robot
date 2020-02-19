bb4f6e745616fbbf28487c36ad66b5e75f3679fe
stem-alliance-team <59984676+stem-alliance-team@users.noreply.github.com>
2020-02-18 19:04:30 -0600

    finish Raise Hook sub-system

--- 7048/src/main/java/frc/robot/subsystems/RaiseHook.java
+++ 7048/src/main/java/frc/robot/subsystems/RaiseHook.java
@@ -7,6 +7,7 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RaiseHook extends SubsystemBase {
@@ -14,10 +15,20 @@ public class RaiseHook extends SubsystemBase {
    * Creates a new ExampleSubsystem.
    */
    private DoubleSolenoid raisePiston;
+   private  boolean up;
   public RaiseHook() {
-    raisePiston =  new DoubleSolenoid()
+   up = true;
+    raisePiston =  new DoubleSolenoid(0, 0);
   }
 
+  public void raise(){ 
+    if(up){
+    raisePiston.set(DoubleSolenoid.Value.kForward);
+  }
+  else{
+   raisePiston.set(DoubleSolenoid.Value.kReverse) ;
+  }
+}
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
